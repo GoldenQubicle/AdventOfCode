c87afa3eb38883041d52b1370bda8783236a7371
vrtxt <goldenqubicle@gmail.com>
2020-12-01 18:33:44 +0100

    part 1 as linq

--- AoC2020/Solutions/Day01.cs
+++ AoC2020/Solutions/Day01.cs
@@ -4,35 +4,26 @@ namespace AoC2020
 {
     public class Day01 : Solution
     {
-        
+
         public Day01(string day) : base(day)
         {
         }
 
-        public override int SolvePart1()
+        public override int SolvePart1( ) => Input.Select(int.Parse)
+            .Where(i => Input.Contains(( 2020 - i ).ToString( )))
+            .Select(i => i * ( 2020 - i )).First( );
+
+
+        public override int SolvePart2( )
         {
             var input = Input.Select(int.Parse);
-            foreach (var number in input)
-            {
-                var lookingFor = 2020 - number;
-                if (Input.Contains(lookingFor.ToString()))
-                {
-                    return lookingFor * number;
-                }
-            }
-            return 0;
-        }
 
-        public override int SolvePart2()
-        {
-            var input = Input.Select(int.Parse).ToList();
-            
-            foreach(var number in input )
+            foreach ( var number in input )
             {
                 var lookingFor1 = 2020 - number;
-                var subset = input.Where(i => i < lookingFor1 && i != number).ToList( );
-                
-                foreach(var other in subset )
+                var subset = input.Where(i => i < lookingFor1 && i != number);
+
+                foreach ( var other in subset )
                 {
                     var lookingFor2 = lookingFor1 - other;
                     if ( input.Contains(lookingFor2) )
