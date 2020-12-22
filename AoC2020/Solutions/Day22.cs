11dac2465acf7eafbe41fe3087a903479ee69126
vrtxt <goldenqubicle@gmail.com>
2020-12-22 12:15:43 +0100

    formatting

--- AoC2020/Solutions/Day22.cs
+++ AoC2020/Solutions/Day22.cs
@@ -22,23 +22,16 @@ namespace AoC2020.Solutions
                 var c2 = player2.Dequeue( );
 
                 if ( c1 > c2 )
-                {
-                    player1.Enqueue(c1);
-                    player1.Enqueue(c2);
-                }
+                    player1.AddCards(c1, c2);
                 else
-                {
-                    player2.Enqueue(c2);
-                    player2.Enqueue(c1);
-                }
+                    player2.AddCards(c2, c1);
             }
-
             return player1.Count > 0 ? player1.GetScore( ) : player2.GetScore( );
         }
 
         public override int SolvePart2( )
         {
-            var (player1, player2) = InitializeGame( );            
+            var (player1, player2) = InitializeGame( );
             return RecurseGame(player1, player2) ? player1.GetScore( ) : player2.GetScore( );
         }
 
@@ -50,7 +43,7 @@ namespace AoC2020.Solutions
             while ( player1.Count > 0 && player2.Count > 0 )
             {
                 if ( OrderSeenBefore(state, player1, player2) ) return true;
-             
+
                 CaptureState(state, player1, player2);
 
                 player1Wins = false;
@@ -58,37 +51,24 @@ namespace AoC2020.Solutions
                 var c2 = player2.Dequeue( );
 
                 if ( player1.Count >= c1 && player2.Count >= c2 )
-                {
                     player1Wins = RecurseGame(player1.NewDeck(c1), player2.NewDeck(c2));
-                }
                 else if ( c1 > c2 )
-                {
                     player1Wins = true;
-                }
 
                 if ( player1Wins )
-                {
-                    player1.Enqueue(c1);
-                    player1.Enqueue(c2);
-                }
+                    player1.AddCards(c1, c2);
                 else
-                {
-                    player2.Enqueue(c2);
-                    player2.Enqueue(c1);
-                }
+                    player2.AddCards(c2, c1);
             }
-
             return player1Wins;
         }
 
         private bool OrderSeenBefore((List<int[ ]> player1, List<int[ ]> player2) state,
             Queue<int> player1, Queue<int> player2) =>
-            state.player1
-                .Where(s => s.Length == player1.Count)
+            state.player1.Where(s => s.Length == player1.Count)
                 .Select(s => s.Select((c, i) => player1.ToArray( )[i] == c))
                 .Any(s => s.All(b => b)) ||
-            state.player2
-                .Where(s => s.Length == player2.Count)
+            state.player2.Where(s => s.Length == player2.Count)
                 .Select(s => s.Select((c, i) => player2.ToArray( )[i] == c))
                 .Any(s => s.All(b => b));
 
@@ -105,5 +85,11 @@ namespace AoC2020.Solutions
         public static Queue<int> NewDeck(this Queue<int> deck, int amount) => new Queue<int>(deck.Take(amount));
 
         public static int GetScore(this Queue<int> deck) => deck.Reverse( ).Select((c, i) => c * ( i + 1 )).Sum( );
+
+        public static void AddCards(this Queue<int> deck, int c1, int c2)
+        {
+            deck.Enqueue(c1);
+            deck.Enqueue(c2);
+        }
     }
 }
