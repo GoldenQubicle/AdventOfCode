4cddf0afb0144e229ebff603589c7825a790dd5f
vrtxt <goldenqubicle@gmail.com>
2020-12-26 16:59:34 +0100

    test case for actual results per day 2020

--- AoC2020Tests/AllDaysResultsTest.cs
+++ AoC2020Tests/AllDaysResultsTest.cs
@@ -10,39 +10,49 @@ namespace AoC2020Tests
 {
     class AllDaysResultsTest
     {
-        [SetUp]
-        public void Setup( )
-        {
-            var results = File.ReadAllLines($"{Directory.GetCurrentDirectory( )}/data/AllResults.txt")
+        //so this is superduper fugly, however, what's more ridiculous is that NUnit FIRST grabs a tescase BEFORE calling SetUp
+        //meaning initializing the testcase source in SetUp will result in a big fat NRE. 
+        private static Dictionary<string, (string pt1, string pt2)> expectedDayResults =>
+            File.ReadAllLines($"{Directory.GetCurrentDirectory( )}/data/AllResults.txt")
                 .Select((line, i) => (line.Split("part"), i))
                 .Aggregate(new Dictionary<string, (string pt1, string pt2)>( ), (dic, line) =>
-                 {
-                     var day = line.Item1[0].Replace(" ", "");
-                     if ( line.i % 2 == 0 )
-                         dic.Add(day, (line.Item1[1][4..], string.Empty));
-                     else
-                         dic[day] = (dic[day].pt1, line.Item1[1][4..]);
-                     return dic;
-                 });
+                {
+                    var day = line.Item1[0].Replace(" ", "");
+                    if ( line.i % 2 == 0 )
+                        dic.Add(day, (line.Item1[1][4..], string.Empty));
+                    else
+                        dic[day] = (dic[day].pt1, line.Item1[1][4..]);
+                    return dic;
+                });
 
-            var assembly = Assembly.LoadFrom($"{Directory.GetCurrentDirectory( )}/AoC2020.dll");
-            var dayType = assembly.GetType("AoC2020.Day01");
+        private static readonly Assembly assembly = Assembly.LoadFrom($"{Directory.GetCurrentDirectory( )}/AoC2020.dll");
+
+
+        [TestCaseSource(nameof(GetExpectedPerDay))]
+        public void ActualResultPerDay(KeyValuePair<string, (string pt1, string pt2)> testCase)
+        {
+            var solution = GetSolutionForDay(testCase.Key);
+            var actualPt1 = solution.SolvePart1( );
+            var actualPt2 = solution.SolvePart2( );
+
+            Assert.AreEqual(testCase.Value.pt1, actualPt1);
+            Assert.AreEqual(testCase.Value.pt2, actualPt2);
+        }
+
+        private static IEnumerable<KeyValuePair<string, (string, string)>> GetExpectedPerDay( )
+        {
+            foreach ( var kvp in expectedDayResults )
+            {
+                yield return kvp;
+            }
+        }
+
+        private Solution GetSolutionForDay(string day)
+        {
+            var dayType = assembly.GetType($"AoC2020.{day}");
             var ctorType = new Type[ ] { typeof(string) };
             var ctor = dayType.GetConstructor(ctorType);
-            var newDay = ( Solution ) ctor.Invoke(new object[ ] { "Day01" });
-
-            var actualPt1 = newDay.SolvePart1( );
-            var actualPt2 = newDay.SolvePart2( );
-
-            Assert.AreEqual(results["Day01"].pt1, actualPt1);
-            Assert.AreEqual(results["Day01"].pt2, actualPt2);
+            return ( Solution ) ctor.Invoke(new object[ ] { day });
         }
-
-        [Test]
-        public void AllDays( )
-        {
-
-        }
-
     }
 }
