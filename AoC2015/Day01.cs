4f91ffe77d4cd794542fe87f512f498e8e4d55c9
vrtxt <goldenqubicle@gmail.com>
2020-12-27 22:01:29 +0100

    2015 day 1

---
+++ AoC2015/Day01.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace AoC2015
+{
+    public class Day01 : Solution
+    {
+        public Day01(string file) : base(file) { }
+
+        public override string SolvePart1( ) => Input[0].Aggregate(0, (sum, c) =>
+            c switch
+                {
+                    '(' => sum+=1,
+                    ')' => sum-=1
+                }
+        ).ToString( );
+
+        public override string SolvePart2( )
+        {
+            var floor = 0;
+            for(int i = 0 ; i < Input[0].Length ; i++ )
+            {
+                floor =  Input[0][i] switch
+                {
+                    '(' => floor += 1,
+                    ')' => floor -= 1
+                };
+                if ( floor == -1 )
+                    return (i+1).ToString( );
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ AoC2015Tests/Day01Test.cs
@@ -0,0 +1,35 @@
+using AoC2015;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2015Tests
+{
+    public class Day01Test
+    {
+        Day01 day01;
+
+        [SetUp]
+        public void Setup( )
+        {
+            day01 = new Day01("day01test1");
+        }
+
+        [TestCase("day01test1", "0")]
+        [TestCase("day01test2", "-3")]
+        public void Part1(string input, string expected)
+        {
+            day01 = new Day01(input);
+            var actual = day01.SolvePart1( );
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Part2( )
+        {
+            day01 = new Day01("day01test3");
+            var actual = day01.SolvePart2( );
+            Assert.AreEqual("5", actual);
+        }
+    }
+}
