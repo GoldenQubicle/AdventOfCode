c32225fb2905647f061a7579a77867f5ec7e7bc2
vrtxt <goldenqubicle@gmail.com>
2020-12-27 10:58:58 +0100

    scaffoling day & unit test

--- App/Program.cs
+++ App/Program.cs
@@ -6,12 +6,26 @@ public class Program
 {
     public static async Task<int> Main(string[ ] args)
     {
+        /*
+         * arguments coming in are expected to be day year, with optional expected value for part1 unit test
+         * example usage; dotnet run --project .\App.csproj 7 2015 1568746
+         * 
+         * TODO find a proper CLI framework       
+         */
+        if ( args.Length >= 2 && 
+            int.TryParse(args[0], out var day) &&
+            int.TryParse(args[1], out var year) )
+        {
+            Console.WriteLine($"Trying to scaffold for year {year} day {day}");
+            int pt1 = 0;
+            if ( args.Length == 3 && int.TryParse(args[2], out pt1) )
+            {
+                Console.WriteLine($"Unit test part 1 has expected value of {pt1}");
+            }
 
-        
+            var template = new SolutionTemplate(year, day).WithUnitTest(pt1);
+        }
 
-        var template = new SolutionTemplate(2015, 1);
-
-        Console.ReadLine( );
         return await Task.FromResult(0);
-    }    
+    }
 }
--- Common/SolutionTemplate.cs
+++ Common/SolutionTemplate.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Common
 {
@@ -18,25 +15,41 @@ namespace Common
 
             Year = year.ToString( ); ;
             Day = day < 10 ? day.ToString( ).PadLeft(2, '0') : day.ToString( );
-
-            var current = Directory.GetCurrentDirectory( ).Replace("App\\bin\\Debug\\net5.0", "");
-            var dir = $"{current}\\AoC{Year}";
-
+            Root = Directory.GetCurrentDirectory( ).Split("\\App")[0];
+            
+            var dir = $"{Root}\\AoC{Year}";
             if ( !Directory.Exists(dir) )
-                throw new InvalidOperationException($"Project for year {year} does not exist yet");
+                throw new InvalidOperationException($"Project for year {Year} does not exist yet");
 
             var path = $"{dir}\\Day{Day}.cs";
-
             if ( File.Exists(path) )
                 throw new InvalidOperationException($"Solution for day {Day} already exists");
 
             File.WriteAllText(path, CreateSolution( ));
         }
 
-        private string Year { get; }
-        private string Day { get; }
+        public SolutionTemplate WithUnitTest(int part1Expected)
+        {
+            var dir = $"{Root}\\AoC{Year}Tests";
 
-        public string CreateSolution( ) =>
+            if ( !Directory.Exists(dir) )
+                throw new InvalidOperationException($"Test Project for year {Year} does not exist yet");
+
+            var path = $"{dir}\\Day{Day}Test.cs";
+
+            if ( File.Exists(path) )
+                throw new InvalidOperationException($"Tests for day {Day} already exists");
+
+            File.WriteAllText(path, CreateUnitTest(part1Expected != 0 ? part1Expected.ToString( ) : string.Empty));
+
+            return this;
+        }
+
+        private string Year { get; }
+        private string Day { get; set; }
+        private string Root { get; }
+
+        private string CreateSolution( ) =>
           $@"using System;
              using System.Collections.Generic;
              using System.Linq;
@@ -50,11 +63,45 @@ namespace Common
                  {{
                      public Day{Day}(string file) : base(file) {{ }}
 
-                     public override string SolvePart1( ) => string.Empty;
+                     public override string SolvePart1( ) => null;
 
-                     public override string SolvePart2( ) => string.Empty;
+                     public override string SolvePart2( ) => null;
                  }}
              }}".Replace("             ", "");
 
+
+        private string CreateUnitTest(string part1Expected) =>
+            $@"using AoC{Year};
+             using NUnit.Framework;
+             using System.Collections.Generic;
+             using System.Linq;
+             
+             namespace AoC{Year}Tests
+             {{
+                 public class Day{Day}Test
+                 {{
+                     Day{Day} day{Day};
+             
+                     [SetUp]
+                     public void Setup( )
+                     {{
+                         day{Day} = new Day{Day}(""day{Day}test1"");
+                     }}
+             
+                     [Test]
+                     public void Part1( )
+                     {{
+                     var actual = day{Day}.SolvePart1( );
+                     Assert.AreEqual(""{( string.IsNullOrEmpty(part1Expected) ? string.Empty : part1Expected )}"", actual);
+                     }}
+             
+                     [Test]
+                     public void Part2( )
+                     {{
+                         var actual = day{Day}.SolvePart2( );
+                         Assert.AreEqual("""", actual);
+                     }}
+                 }}
+             }}".Replace("             ", "");
     }
 }
