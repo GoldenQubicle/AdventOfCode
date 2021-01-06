78a9736b6b4c4d1f8fa9f2517383b9361f1ea3bb
vrtxt <goldenqubicle@gmail.com>
2021-01-06 20:27:48 +0100

    2015 day 10

---
+++ AoC2015/Day10.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Common;
+
+namespace AoC2015
+{
+    public class Day10 : Solution
+    {
+        private readonly Regex pattern = new Regex(@"(?<1>[1]+)|(?<2>[2]+)|(?<3>[3]+)");
+        
+        public Day10(string file) : base(file) { }
+
+        public Day10(List<string> input) : base(input) { }
+
+        public override string SolvePart1( ) => Iterate(40);
+
+        public override string SolvePart2( ) => Iterate(50);
+
+        private string Iterate(int iterations )
+        {
+            var current = Input[0];
+
+            for ( var i = 0 ; i < iterations ; i++ )
+            {
+                current = LookAndSay(current);
+            }
+            return current.Length.ToString( );
+        }
+
+        public string LookAndSay(string input)
+        {
+            var result = new StringBuilder( );
+            foreach ( Match match in pattern.Matches(input) )
+            {
+                if ( match.Groups["1"].Success )
+                {
+                    result.Append(match.Length);
+                    result.Append(1);
+                }
+                if ( match.Groups["2"].Success )
+                {
+                    result.Append(match.Length);
+                    result.Append(2);
+                }
+                if ( match.Groups["3"].Success )
+                {
+                    result.Append(match.Length);
+                    result.Append(3);
+                }
+            }
+            return result.ToString( );
+        }
+    }
+}
---
+++ AoC2015Tests/Day10Test.cs
@@ -0,0 +1,28 @@
+using AoC2015;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AoC2015Tests
+{
+    public class Day10Test
+    {
+        Day10 day10;
+
+        [SetUp]
+        public void Setup( )
+        {
+            day10 = new Day10(new List<string>());
+        }
+        
+        [TestCase("1","11")] 
+        [TestCase("11","21")] 
+        [TestCase("21","1211")] 
+        [TestCase("1211","111221")] 
+        [TestCase("111221","312211")]
+        public void LookAndSayTest(string input, string expected )
+        {
+            var actual = day10.LookAndSay(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
