3ba7e94ad19ca20b0166643d3bcb352cc8683f6c
vrtxt <goldenqubicle@gmail.com>
2021-01-17 20:17:11 +0100

    2015 day 13

--- AoC2015/Day13.cs
+++ AoC2015/Day13.cs
@@ -12,13 +12,13 @@ namespace AoC2015
         public Day13(string file) : base(file, "\n")
         {
             var pattern = new Regex(@"(?<name>[A-Z]\w+)|(?<direction>gain|lose)|(?<unit>\d{1,})");
+           
             Input.ForEach(line =>
             {
                 var matches = pattern.Matches(line);
-                var name = matches.First( ).Value;
-                var other = matches.Last( ).Value;
-                var direction = matches[1].Value;
-                var unit = direction.Equals("gain") ?
+                var name = matches[0].Value;
+                var other = matches[3].Value;
+                var unit = matches[1].Value.Equals("gain") ?
                             int.Parse(matches[2].Value) :
                             int.Parse(matches[2].Value) * -1;
 
@@ -27,9 +27,27 @@ namespace AoC2015
                 else
                     persons[name].Add(other, unit);
             });
+
         }
 
-        public override string SolvePart1( )
+        public override string SolvePart1( ) => FindOptimalArrangement( );
+
+        public override string SolvePart2( )
+        {
+            persons.Add("me", new Dictionary<string, int>( ));
+            persons.ForEach(p =>
+            {
+                if ( !p.Key.Equals("me") )
+                {
+                    persons["me"].Add(p.Key, 0);
+                    p.Value.Add("me", 0);
+                }
+            });
+
+            return FindOptimalArrangement( );
+        }
+
+        private string FindOptimalArrangement( )
         {
             var arrangments = new List<int>( );
             persons.ForEach(person =>
@@ -37,26 +55,18 @@ namespace AoC2015
                 person.Value.ForEach(other =>
                 {
                     var queue = new Queue<(string person, List<string> seated, int score)>( );
-                    queue.Enqueue(( other.Key,
-                        new List<string> { person.Key, other.Key },
-                        persons[person.Key][other.Key] + persons[other.Key][person.Key]));
+                    queue.Enqueue((other.Key, new List<string> { person.Key, other.Key }, GetScore(person.Key, other.Key)));
 
                     while ( queue.Count > 0 )
                     {
                         var current = queue.Dequeue( );
                         var nextMatches = persons[current.person].Where(kvp => !current.seated.Contains(kvp.Key));
+                     
                         if ( nextMatches.Any( ) )
-                        {
-                            nextMatches.ForEach(n =>
-                            {
-                                queue.Enqueue((n.Key,
-                                    current.seated.Expand(n.Key),
-                                    current.score + persons[current.person][n.Key] + persons[n.Key][current.person]));
-                            });
-                        } else
-                        {
-                            arrangments.Add(current.score);
-                        }
+                            nextMatches.ForEach(n => queue.Enqueue((n.Key, current.seated.Expand(n.Key),
+                                    current.score + GetScore(current.person, n.Key))));
+                        else
+                            arrangments.Add(current.score + GetScore(current.person, person.Key));
                     }
                 });
             });
@@ -64,6 +74,7 @@ namespace AoC2015
             return arrangments.Max( ).ToString( );
         }
 
-        public override string SolvePart2( ) => null;
+        private int GetScore(string p1, string p2) => persons[p1][p2] + persons[p2][p1];
+
     }
 }
--- AoC2015Tests/Day13Test.cs
+++ AoC2015Tests/Day13Test.cs
@@ -1,7 +1,5 @@
 using AoC2015;
 using NUnit.Framework;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace AoC2015Tests
 {
@@ -12,21 +10,14 @@ namespace AoC2015Tests
         [SetUp]
         public void Setup( )
         {
-            day13 = new Day13("day13");
+            day13 = new Day13("day13test1");
         }
-        
+
         [Test]
         public void Part1( )
         {
             var actual = day13.SolvePart1( );
             Assert.AreEqual("330", actual);
-        }
-
-        [Test]
-        public void Part2( )
-        {
-            var actual = day13.SolvePart2( );
-            Assert.AreEqual("", actual);
         }
     }
 }
