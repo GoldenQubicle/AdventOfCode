b65ec2aca1180e435c98a5b323f071f707e1626f
vrtxt <goldenqubicle@gmail.com>
2021-01-17 16:30:10 +0100

    2015 day 12 part 2

--- AoC2015/Day12.cs
+++ AoC2015/Day12.cs
@@ -12,51 +12,50 @@ namespace AoC2015
     public class Day12 : Solution
     {
         public Day12(string file) : base(file) { }
-        
+
         public Day12(List<string> input) : base(input) { }
 
         public override string SolvePart1( )
         {
-            var matches = Regex.Matches(Input.First(), @"(?<positive>\d{1,})|(?<negative>-\d{1,})");
+            var matches = Regex.Matches(Input.First( ), @"(?<positive>\d{1,})|(?<negative>-\d{1,})");
             var result = 0;
-            foreach(Match match in matches )
+            foreach ( Match match in matches )
             {
-                result += match.Groups["positive"].Success ? 
-                    int.Parse(match.Groups["positive"].Value) : 
-                    int.Parse(match.Groups["negative"].Value); 
+                result += match.Groups["positive"].Success ?
+                    int.Parse(match.Groups["positive"].Value) :
+                    int.Parse(match.Groups["negative"].Value);
             }
-
-
-            return result.ToString();
+            return result.ToString( );
         }
 
         public override string SolvePart2( )
         {
-            var openings = new Stack<int>( );
-
-            var ranges = Input[0].Select((c,i) =>
-            {
-                var range = (opening: 0, closing: 0);
-                if ( c == '{' ) openings.Push(i);
-                if ( c == '}' ) range = (opening: openings.Pop( ), closing: i);
-                return range;
-            }).Where(r => r.closing != 0)
-            .Select(r => Enumerable.Range(r.opening, r.closing - r.opening)).ToList();
-
-            var reds = Regex.Matches(Input.First( ), @"(?<red>""red"")");
+            var openings = new Stack<(char type, int idx, bool isIgnore)>( );
             var tobeIgnored = new List<IEnumerable<int>>( );
-            foreach(Match match in reds )
+
+            for ( int i = 0 ; i < Input[0].Length ; i++ )
             {
-                var idx = match.Index;
-                var add = ranges.OrderBy(r => r.First( ) - idx).FirstOrDefault( );
-                if(add != default)
-                    tobeIgnored.Add(add);                
-            }
-            tobeIgnored = tobeIgnored.Distinct( ).ToList();
+                var c = Input[0][i];
+
+                if ( c == '{' || c == '[' )
+                    openings.Push((c, i, false));
+
+                if ( c == 'r' && Input[0][i + 1] == 'e' && Input[0][i + 2] == 'd' && openings.Peek( ).type == '{' )
+                {
+                    var opening = openings.Pop( );
+                    openings.Push((opening.type, opening.idx, true));
+                }
+
+                if ( c == '}' || c == ']' )
+                {
+                    var opening = openings.Pop( );
+                    if ( opening.isIgnore )
+                        tobeIgnored.Add(Enumerable.Range(opening.idx, i - opening.idx));
+                }
+            };
 
             var matches = Regex.Matches(Input.First( ), @"(?<positive>\d{1,})|(?<negative>-\d{1,})");
-            var result = 0;
-            foreach ( Match match in matches )
+            var result = matches.Aggregate(0, (result, match) =>
             {
                 if ( !tobeIgnored.Any(r => r.Contains(match.Index)) )
                 {
@@ -64,9 +63,10 @@ namespace AoC2015
                         int.Parse(match.Groups["positive"].Value) :
                         int.Parse(match.Groups["negative"].Value);
                 }
-            }
-
-            return result.ToString();
+                return result;
+            });
+            
+            return result.ToString( );
         }
     }
 }
