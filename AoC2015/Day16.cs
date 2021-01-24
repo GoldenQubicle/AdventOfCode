e8e4aa957a34d20a7686758148ac8bf3b3bf6dac
vrtxt <goldenqubicle@gmail.com>
2021-01-24 19:51:00 +0100

    2015 day 16

---
+++ AoC2015/Day16.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Common;
+
+namespace AoC2015
+{
+    public class Day16 : Solution
+    {
+        private List<Dictionary<string, int>> aunts;
+        private Dictionary<string, int> compounds = new Dictionary<string, int>
+            {
+                { "children", 3},
+                { "cats", 7},
+                { "samoyeds", 2},
+                { "pomeranians", 3},
+                { "akitas", 0},
+                { "vizslas", 0},
+                { "goldfish", 5},
+                { "trees", 3},
+                { "cars", 2},
+                { "perfumes", 1},
+            };
+
+        public Day16(string file) : base(file, "\n")
+        {
+            aunts = Input.Select(line =>
+             {
+                 var matches = Regex.Matches(line,
+                     @"([A-Z]\w+ \d{1,})|(children: \d+)|(cats: \d+)|(samoyeds: \d+)|(pomeranians: \d+)|(akitas: \d+)|(vizslas: \d+)|(goldfish: \d+)|(trees: \d+)|(cars: \d+)|(perfumes: \d+)");
+                 var aunt = new Dictionary<string, int>( );
+                 foreach ( Match match in matches )
+                 {
+                     if ( string.IsNullOrEmpty(match.Value) ) continue;
+
+                     var parts = match.Value.Contains(":") ? match.Value.Split(":") : match.Value.Split(" ");
+                     aunt.Add(parts[0], int.Parse(parts[1]));
+                 }
+                 return aunt;
+             }).ToList( );
+        }
+
+        public override string SolvePart1( )
+        {
+            var potential = compounds.ToDictionary(c => c.Key,
+                c => aunts.Where(a => a.ContainsKey(c.Key) && a[c.Key] == c.Value).Select(a => a["Sue"]).ToList( ));
+
+            return aunts.Where(a => a.Keys.Skip(1).All(k => potential[k].Contains(a["Sue"]))).First( )["Sue"].ToString( );
+
+        }
+
+        public override string SolvePart2( )
+        {
+            var potential = compounds.ToDictionary(c => c.Key,
+                c => aunts.Where(a => a.ContainsKey(c.Key))
+                .Where(a => ( c.Key.Equals("cats") || c.Key.Equals("trees") ) ? a[c.Key] >= c.Value :
+                            ( c.Key.Equals("pomeranians") || c.Key.Equals("goldfish") ) ? a[c.Key] < c.Value : a[c.Key] == c.Value)
+                .Select(a => a["Sue"]).ToList( ));
+
+            return aunts.Where(a => a.Keys.Skip(1).All(k => potential[k].Contains(a["Sue"]))).First( )["Sue"].ToString( );
+        }
+    }
+}
---
+++ AoC2015Tests/Day16Test.cs
@@ -0,0 +1,31 @@
+using AoC2015;
+using NUnit.Framework;
+
+namespace AoC2015Tests
+{
+    public class Day16Test
+    {
+        Day16 day16;
+
+        [SetUp]
+        public void Setup( )
+        {
+            //note no test for this day, using actual input
+            day16 = new Day16("day16");
+        }
+        
+        [Test]
+        public void Part1( )
+        {
+            var actual = day16.SolvePart1( );
+            Assert.AreEqual("103", actual);
+        }
+
+        [Test]
+        public void Part2( )
+        {
+            var actual = day16.SolvePart2( );
+            Assert.AreEqual("405", actual);
+        }
+    }
+}
