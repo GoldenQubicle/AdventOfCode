6db07c80744943229071ec6ec2c5a6c9e68f5eb1
vrtxt <goldenqubicle@gmail.com>
2021-02-01 22:06:57 +0100

    start cellular automaton

---
+++ Common/CellularAutomaton.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public class CellularAutomaton
+    {
+        Dictionary<Position, int> grid;
+
+        public CellularAutomaton(CellularAutomatonOptions options)
+        {
+
+        }
+    }
+
+    public class Position : IEquatable<Position>
+    {
+        public int[ ] Values { get; init; }
+        private int Dimensions;
+
+        public Position(int[] values)
+        {
+            Values = values;
+            Dimensions = Values.Length;
+        }
+
+        public bool Equals(Position other)
+        {
+            if(other.Dimensions != Dimensions) return false;
+
+            bool EqualValue = true;
+            for(var i = 0 ; i < Dimensions ; i++)
+            {
+                if(other.Values[i] != Values[i])
+                {
+                    EqualValue = false;
+                    break;
+                }
+            }
+            return EqualValue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null || obj == default) return false;
+
+            if(obj.GetType() != typeof(Position)) return false;
+
+            return Equals((Position)obj);
+        }
+
+        public override int GetHashCode( )
+        {
+            unchecked
+            {
+                int hash = 17;
+                foreach(var item in Values)
+                {
+                    hash = 31 * hash + item.GetHashCode();
+                }
+                return hash;
+            }
+        }
+
+        public static bool operator ==(Position left, Position right) => EqualityComparer<Position>.Default.Equals(left, right);
+        public static bool operator !=(Position left, Position right) => !(left == right);
+    }
+}
---
+++ CommonTests/CellularAutomatonTest.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonTests
+{
+    public class CellularAutomatonTest
+    {
+        [Test]
+        public void PositionTests( )
+        {
+            var grid = new Dictionary<Position, int>
+            {
+                { new Position( new int[]{ 1, 2 }), 5 },
+                { new Position( new int[]{ 2, 1 }), 15 }
+            };
+
+            var s = grid[new Position(new int[ ] { 2, 1 })];
+            Assert.AreEqual(15, s);
+
+            Assert.IsTrue(grid.Keys.First() == new Position(new int[ ] { 1, 2 }));
+
+            Assert.IsFalse(grid.Keys.First() == null);
+        }
+    }
+}
