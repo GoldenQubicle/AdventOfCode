de48e5602d0405f658d7c8f9d4e972614ff21ead
vrtxt <goldenqubicle@gmail.com>
2021-04-03 10:49:17 +0200

    2016 day 16

---
+++ AoC2016/Day16.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Common;
+
+namespace AoC2016
+{
+    public class Day16 : Solution
+    {
+        public int Length { get; set; } = 272;
+        public string InitialState { get; }
+
+        public Day16(string file) : base(file, "\n") =>  InitialState = Input[0]; 
+        
+        public Day16(List<string> input) : base(input) =>  InitialState = Input[0];
+
+        public override string SolvePart1()
+        {
+            var data = InitialState;
+            while (data.Length < Length)
+                data = Dragonize(data);
+
+            return CheckSum(data[..Length]);
+        }
+
+        public override string SolvePart2()
+        {
+            Length = 35651584;
+            return SolvePart1();
+        }
+
+        public string Dragonize(string input) => @$"{input}{0}{
+            new string(input.Reverse().Select(c => c switch
+            {
+                '1' => '0',
+                '0' => '1',
+                _ => c
+            }).ToArray())}";
+
+        public string CheckSum(string input)
+        {
+            var checksum = GetActualCheckSum(input);
+
+            while(checksum.Length % 2 == 0 )
+                checksum = GetActualCheckSum(checksum);
+
+            return checksum;
+        }
+
+        private static string GetActualCheckSum(string input)
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < input.Length; i += 2)
+            {
+                sb.Append(input[i] == input[i + 1] ? "1" : "0");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ AoC2016Tests/Day16Test.cs
@@ -0,0 +1,52 @@
+using AoC2016;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace AoC2016Tests
+{
+    public class Day16Test
+    {
+        Day16 day16;
+
+        [SetUp]
+        public void Setup( )
+        {
+            day16 = new Day16(new List<string>{ "10000" });
+        }
+
+        [Test]
+        public void Part1( )
+        {
+            day16.Length = 20;
+            var actual = day16.SolvePart1();
+            Assert.AreEqual("01100", actual);
+        }
+
+        [TestCase("1","100")] 
+        [TestCase("0","001")] 
+        [TestCase("11111","11111000000")] 
+        [TestCase("111100001010","1111000010100101011110000")]
+        public void DragonizeTest(string input, string expected )
+        {
+            var actual = day16.Dragonize(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("110010110100", "100")]
+        [TestCase("10000011110010000111", "01100")]
+        public void CheckSumTest(string input, string expected)
+        {
+            var actual = day16.CheckSum(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Part2( )
+        {
+            var actual = day16.SolvePart2( );
+            Assert.AreEqual("", actual);
+        }
+    }
+}
