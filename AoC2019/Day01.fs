ddbc7007fc1ce7354e7525b4e8e6ad787e88d2df
vrtxt <goldenqubicle@gmail.com>
2021-07-10 17:14:12 +0200

    day01 part 1 test cases

---
+++ AoC2019/Day01.fs
@@ -0,0 +1,13 @@
+﻿namespace AoC2019
+
+open System
+open Microsoft.FSharp.Core.Operators
+
+module Day01 = 
+    let SolvePart1 : string = "part 1"
+
+    let SolvePart2 : string = "part 2"
+
+    let GetFuel (mass : float32): float32 =  MathF.Floor( mass / 3f) - 2f
+
+
---
+++ AoC2019Tests/Day01Test.fs
@@ -0,0 +1,25 @@
+﻿module Day01Test
+
+open AoC2019
+
+open NUnit.Framework
+
+[<Test>]
+let Part1 () =
+    let result = Day01.SolvePart1
+    Assert.AreEqual(result, "part 1")
+
+
+[<Test>]
+let Part2 () = 
+    let result = Day01.SolvePart2
+    Assert.AreEqual(result, "part 2")
+
+        
+[<TestCase(12, 2)>]
+[<TestCase(14, 2)>]
+[<TestCase(1969, 654)>]
+[<TestCase(100756, 33583)>]
+let GetFuelTest(input : float32,  expected : float32) =
+    let actual = Day01.GetFuel(input)
+    Assert.AreEqual(expected, actual)
