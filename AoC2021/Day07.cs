130370c2c00d60302d18b7b051c79670924094c2
vrtxt <goldenqubicle@gmail.com>
2021-12-07 18:00:03 +0100

    rewrite as linq and other minor tweaks

--- AoC2021/Day07.cs
+++ AoC2021/Day07.cs
@@ -11,10 +11,10 @@ namespace AoC2021
 
         public Day07(string file) : base(file) => numbers = Input.First().Split(',').Select(int.Parse).ToList();
 
-        public override string SolvePart1( ) => numbers.Select((t, idx) => 
+        public override string SolvePart1( ) => numbers.Select((_, idx) => 
             numbers.Select(n => Math.Abs(n - idx)).Sum()).Min().ToString();
 
-        public override string SolvePart2() => numbers.Select((t, idx) => 
+        public override string SolvePart2() => numbers.Select((_, idx) => 
             numbers.Select(n => Math.Abs(n - idx)).Select(c => Enumerable.Range(1, c).Sum()).Sum()).Min().ToString();
     }
 }
