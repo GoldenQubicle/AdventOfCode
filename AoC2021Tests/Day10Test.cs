e235181649ea2cf58e68e94e8fc05fed47a20fcb
vrtxt <goldenqubicle@gmail.com>
2021-12-10 07:53:24 +0100

    2021 day 10

---
+++ AoC2021Tests/Day10Test.cs
@@ -0,0 +1,30 @@
+using AoC2021;
+using NUnit.Framework;
+
+namespace AoC2021Tests
+{
+    public class Day10Test
+    {
+        Day10 day10;
+
+        [SetUp]
+        public void Setup( )
+        {
+            day10 = new Day10("day10test1");
+        }
+        
+        [Test]
+        public void Part1( )
+        {
+            var actual = day10.SolvePart1( );
+            Assert.AreEqual("26397", actual);
+        }
+
+        [Test]
+        public void Part2( )
+        {
+            var actual = day10.SolvePart2( );
+            Assert.AreEqual("288957", actual);
+        }
+    }
+}
