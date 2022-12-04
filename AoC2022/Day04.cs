ef593ab027d4bf600b9316c172e8fff2d95e4c45
vrtxt <goldenqubicle@gmail.com>
2022-12-04 12:38:50 +0100

    2022 day 4

---
+++ AoC2022/Day04.cs
@@ -0,0 +1,23 @@
+namespace AoC2022
+{
+    public class Day04 : Solution
+    {
+        private readonly List<(int p1Start, int p1End, int p2Start, int p2End)> pairs;
+
+        public Day04(string file) : base(file) => pairs = Input
+            .Select(l => Regex.Match(l, @"(?<p1s>\d+)-(?<p1e>\d+),(?<p2s>\d+)-(?<p2e>\d+)"))
+            .Select(m => (p1Start: int.Parse(m.Groups["p1s"].Value), p1End: int.Parse(m.Groups["p1e"].Value),
+                p2Start: int.Parse(m.Groups["p2s"].Value), p2End: int.Parse(m.Groups["p2e"].Value))).ToList();
+
+        public override string SolvePart1() => pairs
+            .Count(p => (p.p1Start >= p.p2Start && p.p1End <= p.p2End) ||
+                        (p.p2Start >= p.p1Start && p.p2End <= p.p1End)).ToString();
+
+        public override string SolvePart2() => pairs
+            .Count(p => (p.p1Start <= p.p2Start && p.p1End >= p.p2Start) ||
+                        (p.p1Start <= p.p2End   && p.p1End >= p.p2End) ||
+                        (p.p2Start <= p.p1Start && p.p2End >= p.p1Start) ||
+                        (p.p2Start <= p.p1End   && p.p2End >= p.p1End)).ToString();
+
+    }
+}
---
+++ AoC2022Tests/Day04Test.cs
@@ -0,0 +1,27 @@
+namespace AoC2022Tests
+{
+    public class Day04Test
+    {
+        Day04 day04;
+
+        [SetUp]
+        public void Setup( )
+        {
+            day04 = new Day04("day04test1");
+        }
+        
+        [Test]
+        public void Part1( )
+        {
+            var actual = day04.SolvePart1( );
+            Assert.AreEqual("2", actual);
+        }
+
+        [Test]
+        public void Part2( )
+        {
+            var actual = day04.SolvePart2( );
+            Assert.AreEqual("4", actual);
+        }
+    }
+}
