631f1695f860d4ea029a35a9b11d4152149cf4e1
vrtxt <goldenqubicle@gmail.com>
2022-12-04 12:49:12 +0100

    better overlap check

--- AoC2022/Day04.cs
+++ AoC2022/Day04.cs
@@ -7,17 +7,15 @@ namespace AoC2022
         public Day04(string file) : base(file) => pairs = Input
             .Select(l => Regex.Match(l, @"(?<p1s>\d+)-(?<p1e>\d+),(?<p2s>\d+)-(?<p2e>\d+)"))
             .Select(m => (p1Start: int.Parse(m.Groups["p1s"].Value), p1End: int.Parse(m.Groups["p1e"].Value),
-                p2Start: int.Parse(m.Groups["p2s"].Value), p2End: int.Parse(m.Groups["p2e"].Value))).ToList();
+                          p2Start: int.Parse(m.Groups["p2s"].Value), p2End: int.Parse(m.Groups["p2e"].Value))).ToList();
 
         public override string SolvePart1() => pairs
             .Count(p => (p.p1Start >= p.p2Start && p.p1End <= p.p2End) ||
                         (p.p2Start >= p.p1Start && p.p2End <= p.p1End)).ToString();
 
         public override string SolvePart2() => pairs
-            .Count(p => (p.p1Start <= p.p2Start && p.p1End >= p.p2Start) ||
-                        (p.p1Start <= p.p2End   && p.p1End >= p.p2End) ||
-                        (p.p2Start <= p.p1Start && p.p2End >= p.p1Start) ||
-                        (p.p2Start <= p.p1End   && p.p2End >= p.p1End)).ToString();
+            .Count(p => (p.p1Start <= p.p2End && p.p1End >= p.p2Start)).ToString();
+
 
     }
 }
