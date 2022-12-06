9cbfec68ec8f129c2f8b10686c1cd1410701acdc
vrtxt <goldenqubicle@gmail.com>
2022-12-06 07:55:38 +0100

    2022 day 6

---
+++ AoC2022/Day06.cs
@@ -0,0 +1,23 @@
+namespace AoC2022
+{
+    public class Day06 : Solution
+    {
+        public Day06(List<string> input) : base(input) { }
+        
+        public override string SolvePart1() => FindMarker(4);
+        public override string SolvePart2() => FindMarker(14);
+
+
+        private string FindMarker(int markerLength)
+        {
+            for (var i = 0; i < Input[0].Length; i++)
+            {
+                if (Input[0].Substring(i, markerLength).Distinct().Count() == markerLength)
+                {
+                    return (i + markerLength).ToString();
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ AoC2022Tests/Day06Test.cs
@@ -0,0 +1,37 @@
+namespace AoC2022Tests
+{
+    public class Day06Test
+    {
+        Day06 day06;
+
+        [SetUp]
+        public void Setup( )
+        {
+            //day06 = new Day06("day06test1");
+        }
+        
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", "7")]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", "5")]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg","6")] 
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg","10")] 
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw","11")]
+        public void Part1(string input, string expected )
+        {
+            day06 = new Day06(new List<string> { input } );
+            var actual = day06.SolvePart1( );
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", "19")]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", "23")]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", "23")]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "29")]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "26")]
+        public void Part2(string input, string expected)
+        {
+            day06 = new Day06(new List<string> { input });
+            var actual = day06.SolvePart2( );
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
