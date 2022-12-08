c515dc6727169c25494e636b31e8c86f1b3b49d9
vrtxt <goldenqubicle@gmail.com>
2022-12-08 09:49:17 +0100

    small linq refactor day 8 2022

--- AoC2022/Day08.cs
+++ AoC2022/Day08.cs
@@ -8,65 +8,53 @@ namespace AoC2022
 
         public Day08(string file) : base(file) => grid = new Grid2d(Input, diagonalAllowed: false);
 
-        public override string SolvePart1()
-        {
-            var totalVisible = grid.Count;
-
-            foreach (var cell in grid.Where(FilterEdge))
+        public override string SolvePart1() => grid.Where(FilterEdge)
+            .Aggregate(grid.Count, (visible, cell) =>
             {
                 var hiddenLeft = IsHidden(c => c.X < cell.X && c.Y == cell.Y, cell.Value);
                 var hiddenRight = IsHidden(c => c.X > cell.X && c.Y == cell.Y, cell.Value);
                 var hiddenUp = IsHidden(c => c.X == cell.X && c.Y < cell.Y, cell.Value);
                 var hiddenDown = IsHidden(c => c.X == cell.X && c.Y > cell.Y, cell.Value);
 
-                if (hiddenLeft && hiddenRight && hiddenDown && hiddenUp)
-                    totalVisible--;
-            }
+                return (hiddenLeft && hiddenRight && hiddenUp && hiddenDown) ? --visible : visible;
+            }).ToString();
 
-            return totalVisible.ToString();
-        }
-
-        public override string SolvePart2()
-        {
-            var scores = new Dictionary<Cell, int>();
-
-            foreach (var cell in grid.Where(FilterEdge))
+        public override string SolvePart2() => grid.Where(FilterEdge)
+            .Select(cell =>
             {
-                var scoreLeft = WalkLine(cell, i => (cell.X - i, cell.Y));
-                var scoreRight = WalkLine(cell, i => (cell.X + i, cell.Y));
-                var scoreUp = WalkLine(cell, i => (cell.X, cell.Y - i));
-                var scoreDown = WalkLine(cell, i => (cell.X, cell.Y + i));
+                var scoreLeft = WalkLine(step => (cell.X - step, cell.Y), cell.Value);
+                var scoreRight = WalkLine(step => (cell.X + step, cell.Y), cell.Value);
+                var scoreUp = WalkLine(step => (cell.X, cell.Y - step), cell.Value);
+                var scoreDown = WalkLine(step => (cell.X, cell.Y + step), cell.Value);
 
-                scores.Add(cell, scoreLeft * scoreRight * scoreUp * scoreDown);
-            }
+                return scoreLeft * scoreRight * scoreUp * scoreDown;
+            }).Max().ToString();
 
-            return scores.Values.Max().ToString();
-        }
 
-        private bool IsHidden(Func<Cell, bool> direction, long value) => 
+        private bool IsHidden(Func<Cell, bool> direction, long value) =>
             grid.GetCells(direction).Any(c => c.Value >= value);
 
         private bool FilterEdge(Cell cell) =>
             cell.X >= 1 && cell.X < grid.Dimensions.x &&
             cell.Y >= 1 && cell.Y < grid.Dimensions.y;
 
-        private int WalkLine(Cell cell, Func<int, (int x, int y)> getNextPosition)
+        private int WalkLine(Func<int, (int x, int y)> getNextPosition, long value)
         {
             var isBlocked = false;
-            var i = 1;
+            var step = 1;
 
             while (!isBlocked)
             {
-                var (x, y) = getNextPosition(i);
+                var (x, y) = getNextPosition(step);
                 if (x < 0 || x >= grid.Dimensions.x ||
                     y < 0 || y >= grid.Dimensions.y)
-                    return i - 1;
+                    return step - 1;
 
-                if (grid[x, y].Value < cell.Value) i++;
+                if (grid[x, y].Value < value) step++;
                 else isBlocked = true;
             }
 
-            return i;
+            return step;
         }
     }
 }
