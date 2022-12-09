32e1857c1d8ce062f2763accda150c45a68c6050
vrtxt <goldenqubicle@gmail.com>
2022-12-09 15:00:51 +0100

    2022 day 9

--- AoC2022/Day09.cs
+++ AoC2022/Day09.cs
@@ -2,62 +2,44 @@ namespace AoC2022
 {
     public class Day09 : Solution
     {
-        public Day09(string file) : base(file) { }
-        
-        public Day09(List<string> input) : base(input) { }
+        private readonly List<(string direction, int steps)> motions;
 
-        public override string SolvePart1()
-        {
-            var head = (x: 0, y: 0);
-            var tail = (x: 0, y: 0);
-            var visited = new HashSet<(int, int)> { tail };
+        public Day09(string file) : base(file) => motions = Input
+            .Select(l => l.Split(" "))
+            .Select(p => (direction: p[0], steps: int.Parse(p[1]))).ToList();
 
-            foreach (var step in Input)
+        public override string SolvePart1() => SimulateRope(GetRope(2));
+
+        public override string SolvePart2() => SimulateRope(GetRope(10));
+
+        private string SimulateRope(List<(int x, int y)> rope) =>
+            motions.Aggregate(new HashSet<(int, int)> { rope.Last() }, (visited, motion) =>
             {
-                var parts = step.Split(" ");
-                var current = parts[0];
-
-                for (var i = 0; i < int.Parse(parts[1]); i++)
+                for (var s = 0; s < motion.steps; s++)
                 {
-                    head = current switch
+                    rope[0] = motion.direction switch
                     {
-                        "R" => head.Add(1, 0),
-                        "L" => head.Add(-1, 0),
-                        "U" => head.Add(0, 1),
-                        "D" => head.Add(0, -1),
+                        "R" => rope[0].Add(1, 0),
+                        "L" => rope[0].Add(-1, 0),
+                        "U" => rope[0].Add(0, 1),
+                        "D" => rope[0].Add(0, -1),
                     };
 
-                    if(OverLaps(head, tail)) continue;
+                    for (var k = 1; k < rope.Count; k++)
+                        rope[k] = UpdateKnot(rope[k - 1], rope[k]);
 
-                    tail = current switch
-                    {
-                        "R" when head.y < tail.y => tail.Add(1, -1),
-                        "R" when head.y > tail.y => tail.Add(1, 1),
-                        "R" => tail.Add(1, 0),
-                        "L" when head.y < tail.y => tail.Add(-1, -1),
-                        "L" when head.y > tail.y => tail.Add(-1, 1),
-                        "L" => tail.Add(-1, 0),
-                        "U" when head.x < tail.x => tail.Add(-1, 1),
-                        "U" when head.x > tail.x => tail.Add(1, 1),
-                        "U"  => tail.Add(0, 1),
-                        "D" when head.x < tail.x => tail.Add(-1, -1),
-                        "D" when head.x > tail.x => tail.Add(1, -1),
-                        "D"  => tail.Add(0, -1),
-                    };
-
-                    visited.Add(tail);
+                    visited.Add(rope.Last());
                 }
+                return visited;
+            }).Count.ToString();
 
-                
-            }
-            return visited.Count.ToString();
-        }
+        private static (int x, int y) UpdateKnot((int x, int y) head, (int x, int y) tail) =>
+            Math.Abs(head.x - tail.x) > 1 || Math.Abs(head.y - tail.y) > 1
+                ? tail.Add(Math.Sign(head.x - tail.x), Math.Sign(head.y - tail.y))
+                : tail;
 
-        public static bool OverLaps((int x, int y) head, (int x, int y) tail) => 
-            tail.x >= head.x - 1 && tail.x <= head.x + 1 &&
-            tail.y >= head.y - 1 && tail.y <= head.y + 1;
+        private List<(int x, int y)> GetRope(int length) =>
+            Enumerable.Range(0, length).Select(n => (x: 0, y: 0)).ToList();
 
-
-        public override string SolvePart2( ) => null;
     }
 }
--- AoC2022Tests/Day09Test.cs
+++ AoC2022Tests/Day09Test.cs
@@ -3,16 +3,11 @@ namespace AoC2022Tests
     public class Day09Test
     {
         Day09 day09;
-
-        [SetUp]
-        public void Setup()
-        {
-            day09 = new Day09("day09test1");
-        }
-
+        
         [Test]
         public void Part1()
         {
+            day09 = new Day09("day09test1");
             var actual = day09.SolvePart1();
             Assert.AreEqual("13", actual);
         }
@@ -20,19 +15,9 @@ namespace AoC2022Tests
         [Test]
         public void Part2()
         {
+            day09 = new Day09("day09test2");
             var actual = day09.SolvePart2();
-            Assert.AreEqual("", actual);
-        }
-
-        [TestCase(0, 0, 0, 0, true)]
-        [TestCase(1, 1, 0, 0, true)]
-        [TestCase(2, 1, 0, 0, false)]
-        [TestCase(1, 2, 0, 0, false)]
-        [TestCase(1, -2, 1, -1, true)]
-        public void OverlapTest(int hx, int hy, int tx, int ty, bool expected)
-        {
-            var actual = Day09.OverLaps((hx, hy), (tx, ty));
-            Assert.AreEqual(actual, expected);
+            Assert.AreEqual("36", actual);
         }
     }
 }
