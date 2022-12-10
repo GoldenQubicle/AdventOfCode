e2057925ade8cd1c7682f826549d037dc7daec1f
vrtxt <goldenqubicle@gmail.com>
2022-12-10 10:23:47 +0100

    refactor 2022 day 10

--- AoC2022/Day10.cs
+++ AoC2022/Day10.cs
@@ -2,65 +2,59 @@ namespace AoC2022
 {
     public class Day10 : Solution
     {
-        private readonly IEnumerable<(string, long)> instructions;
+        private readonly IEnumerable<(string, int)> instructions;
 
-        public Day10(string file) : base(file) => instructions = Input.Select(l => 
-            l.StartsWith("noop") ? ("noop", 0) : (l.Split(" ")[0], long.Parse(l.Split(" ")[1])));
+        private readonly List<int> cyclesToCheck = new() { 20, 60, 100, 140, 180, 220 };
+        private readonly List<int> signals = new();
+        private readonly StringBuilder crt = new();
+        public Day10(string file) : base(file) => instructions = Input.Select(l =>
+            l.StartsWith("noop") ? ("noop", 0) : (l.Split(" ")[0], int.Parse(l.Split(" ")[1])));
 
+        public override string SolvePart1() => DoCycles(isPart1: true);
 
-        public override string SolvePart1()
+        public override string SolvePart2() => DoCycles(isPart1: false);
+
+        private string DoCycles(bool isPart1)
         {
-            var x = 1L;
+            var x = 1;
             var cycle = 0;
-            var signals = new List<long>();
-            var cyclesToCheck = new List<int> { 20, 60, 100, 140, 180, 220 };
+
             foreach (var (op, v) in instructions)
             {
-                cycle++;
-
-                if (cyclesToCheck.Contains(cycle))
-                    signals.Add(cycle * x);
+                cycle = isPart1 ? DoCyclePart1(cycle, x) : DoCyclePart2(cycle, x);
 
                 if (op.Equals("noop"))
                     continue;
 
-                cycle++;
-
-                if (cyclesToCheck.Contains(cycle))
-                    signals.Add(cycle * x);
+                cycle = isPart1 ? DoCyclePart1(cycle, x) : DoCyclePart2(cycle, x);
 
                 x += v;
-
             }
 
-            return signals.Sum().ToString();
+            return isPart1 ? signals.Sum().ToString() : crt.ToString();
         }
 
-        public override string SolvePart2()
+        private int DoCyclePart1(int cycle, int x)
         {
-            var x = 1L;
-            var cycle = 0;
-            var crt = new StringBuilder();
+            cycle++;
 
-            List<long> GetSpritePosition(long idx) => new() { idx - 1, idx, idx + 1 };
+            if (cyclesToCheck.Contains(cycle))
+                signals.Add(cycle * x);
 
-            foreach (var (op, v) in instructions)
-            {
-                if(cycle % 40 == 0) crt.Append(Environment.NewLine);
-                crt.Append(GetSpritePosition(x).Contains(cycle % 40) ? "#" : ".");
-                cycle++;
-                
-                if (op.Equals("noop"))
-                    continue;
-
-                if (cycle % 40 == 0) crt.Append(Environment.NewLine);
-                crt.Append(GetSpritePosition(x).Contains(cycle % 40) ? "#" : ".");
-                cycle++;
-
-                x += v;
-            }
-
-            return crt.ToString();
+            return cycle;
         }
+
+        private int DoCyclePart2(int cycle, int x)
+        {
+            if (cycle % 40 == 0)
+                crt.Append(Environment.NewLine);
+
+            crt.Append(GetSpritePosition(x).Contains(cycle % 40) ? "#" : ".");
+
+            return ++cycle;
+        }
+
+        private static List<long> GetSpritePosition(long idx) => new() { idx - 1, idx, idx + 1 };
+
     }
 }
