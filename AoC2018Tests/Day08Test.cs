82114dffab1d2c9a32b173024e41fa77e329c824
vrtxt <goldenqubicle@gmail.com>
2023-11-28 16:13:08 +0100

    2018 day 8

---
+++ AoC2018Tests/Day08Test.cs
@@ -0,0 +1,32 @@
+using AoC2018;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2018Tests
+{
+    public class Day08Test
+    {
+        Day08 day08;
+
+        [SetUp]
+        public void Setup( )
+        {
+            day08 = new Day08("day08test1");
+        }
+        
+        [Test]
+        public void Part1( )
+        {
+            var actual = day08.SolvePart1( );
+            Assert.That(actual, Is.EqualTo("138"));
+        }
+
+        [Test]
+        public void Part2( )
+        {
+            var actual = day08.SolvePart2( );
+            Assert.That(actual, Is.EqualTo("66"));
+        }
+    }
+}
