ca1c42fde0e7ee41c1d29005da8d445cd184ff42
vrtxt <goldenqubicle@gmail.com>
2023-12-01 07:26:07 +0100

    2023 day 1

---
+++ AoC2023Tests/Day01Test.cs
@@ -0,0 +1,23 @@
+using AoC2023;
+
+namespace AoC2023Tests
+{
+    public class Day01Test
+    {
+        [Test]
+        public void Part1( )
+        {
+	        var day01 = new Day01("day01test1");
+			var actual = day01.SolvePart1( );
+            Assert.That(actual, Is.EqualTo("142"));
+        }
+
+        [Test]
+        public void Part2( )
+        {
+	        var day01 = new Day01("day01test2");
+            var actual = day01.SolvePart2( );
+            Assert.That(actual, Is.EqualTo("281"));
+        }
+    }
+}
