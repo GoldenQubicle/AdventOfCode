0926132c507f0e24e8cfb2afa4af250b167d0e19
vrtxt <goldenqubicle@gmail.com>
2023-12-04 07:31:29 +0100

    2023 day 4

---
+++ AoC2023/Day04.cs
@@ -0,0 +1,44 @@
+using Common;
+using Common.Extensions;
+
+namespace AoC2023
+{
+    public class Day04 : Solution
+    {
+	    private readonly Dictionary<int, (List<int> win, List<int> numbers )> cards;
+
+	    public Day04(string file) : base(file) => cards = Input
+		    .Select((l, idx) => (l.Split(':')[1], idx))
+		    .ToDictionary(t => t.idx + 1, t =>
+		    {
+			    var parts = t.Item1.Split('|');
+				return (
+					parts[0].Split(' ').Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList(), 
+					parts[1].Split(' ').Select(s => s.Trim( )).Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse).ToList());
+		    });
+        
+
+        public override string SolvePart1( ) => cards.Values
+	        .Select(c => c.numbers.Intersect(c.win))
+	        .Select(i => Math.Pow(2, i.Count( )) / 2)
+	        .Where(s => s >= 1)
+	        .Sum( ).ToString( );
+
+
+		public override string SolvePart2( )
+		{
+			var wins = cards.ToDictionary(c => c.Key, c => c.Value.numbers.Intersect(c.Value.win).Count());
+			var queue = new Queue<int>();
+			var totalCards = 0;
+			cards.Keys.ForEach(queue.Enqueue);
+			while (queue.Count != 0)
+			{
+				totalCards++;
+				var current = queue.Dequeue();
+				Enumerable.Range(current + 1, wins[current]).ForEach(queue.Enqueue);
+			}
+
+			return totalCards.ToString();
+        }
+    }
+}
---
+++ AoC2023Tests/Day04Test.cs
@@ -0,0 +1,29 @@
+using AoC2023;
+
+namespace AoC2023Tests
+{
+    public class Day04Test
+    {
+        Day04 day04;
+
+        [SetUp]
+        public void Setup( )
+        {
+            day04 = new Day04("day04test1");
+        }
+        
+        [Test]
+        public void Part1( )
+        {
+            var actual = day04.SolvePart1( );
+            Assert.That(actual, Is.EqualTo("13"));
+        }
+
+        [Test]
+        public void Part2( )
+        {
+            var actual = day04.SolvePart2( );
+            Assert.That(actual, Is.EqualTo("30"));
+        }
+    }
+}
