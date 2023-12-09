b2e9fd9da73dcf8f34887a78b41df878634ea590
vrtxt <goldenqubicle@gmail.com>
2023-12-09 07:43:56 +0100

    2023 day 9

---
+++ AoC2023/Day09.cs
@@ -0,0 +1,56 @@
+using Common;
+
+namespace AoC2023;
+
+public class Day09 : Solution
+{
+
+	private readonly List<List<long>> reports;
+
+	public Day09(string file) : base(file) => reports = Input
+		.Select(l => l.Split(' ', StringSplitOptions.TrimEntries).Select(long.Parse).ToList( )).ToList( );
+
+	public override string SolvePart1() => GetInterpolatedSum(isPart2: false);
+
+	public override string SolvePart2() => GetInterpolatedSum(isPart2: true);
+	
+	private string GetInterpolatedSum(bool isPart2)
+	{
+		var result = new List<long>( );
+		foreach (var report in reports)
+		{
+			if (isPart2)
+				report.Reverse();
+
+			var last = new Stack<long>( );
+			last.Push(report.Last( ));
+			var delta = GetDifferences(report);
+
+			while (delta.Any(d => d != 0))
+			{
+				last.Push(delta.Last( ));
+				delta = GetDifferences(delta);
+			}
+
+			var interpolated = last.Pop() + last.Pop();
+			while (last.Count != 0)
+			{
+				interpolated += last.Pop();
+			}
+			result.Add(interpolated);
+		}
+
+		return result.Sum( ).ToString( );
+	}
+
+	private List<long> GetDifferences(List<long> input)
+	{
+		var result = new List<long>( );
+		for (var i = 0 ;i < input.Count - 1 ;i++)
+		{
+			result.Add(input[i + 1] - input[i]);
+		}
+
+		return result;
+	}
+}
---
+++ AoC2023Tests/Day09Test.cs
@@ -0,0 +1,31 @@
+using AoC2023;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2023Tests;
+            
+public class Day09Test
+{
+    Day09 day09;
+            
+    [SetUp]
+    public void Setup( )
+    {
+        day09 = new Day09("day09test1");
+    }
+    
+    [Test]
+    public void Part1( )
+    {
+        var actual = day09.SolvePart1( );
+        Assert.That(actual, Is.EqualTo("114"));
+    }
+            
+    [Test]
+    public void Part2( )
+    {
+        var actual = day09.SolvePart2( );
+        Assert.That(actual, Is.EqualTo("2"));
+    }
+}
