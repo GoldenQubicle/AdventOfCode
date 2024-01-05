e7a6354f92c244d970971c94999b2d0de3ff4933
vrtxt <goldenqubicle@gmail.com>
2024-01-05 21:41:14 +0100

    cli to filescoped namespace

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -5,34 +5,35 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
-namespace CLI
+namespace CLI;
+
+public class Program
 {
-	public class Program
+	public static async Task Main(string[ ] args) =>
+		await Parser.Default.ParseArguments(args, LoadVerbs( )).WithParsedAsync(Run);
+
+	private static Task Run(object obj) => obj switch
 	{
-		public static async Task Main(string[ ] args) =>
-				await Parser.Default.ParseArguments(args, LoadVerbs( )).WithParsedAsync(Run);
-
-		private static Task Run(object obj) => obj switch
+		ScaffoldOptions s => Task.Run(async ( ) =>
 		{
-			ScaffoldOptions s => Task.Run(async ( ) =>
-			{
-				Console.WriteLine(await GetInputOptions.Run(await s.ToGetInputOption( )));
-				Console.WriteLine(await ScaffoldOptions.Run(s));
-				Console.ResetColor( );
-			}),
-			RunDayOptions r => Task.Run(async ( ) =>
-			{
-				Console.WriteLine(await RunDayOptions.Run(r));
-				Console.ResetColor( );
-			}),
-			GetInputOptions i => Task.Run(async ( ) =>
-		   {
-			   Console.WriteLine(await GetInputOptions.Run(i));
-			   Console.ResetColor( );
-		   }),
-		};
+			Console.WriteLine(await GetInputOptions.Run(await s.ToGetInputOption( )));
+			Console.WriteLine(await ScaffoldOptions.Run(s));
+			Console.ResetColor( );
+		}),
 
-		private static Type[ ] LoadVerbs() => Assembly.GetExecutingAssembly( ).GetTypes( )
-				.Where(t => t.GetCustomAttribute<VerbAttribute>( ) != null).ToArray( );
-	}
+		RunDayOptions r => Task.Run(async ( ) =>
+		{
+			Console.WriteLine(await RunDayOptions.Run(r));
+			Console.ResetColor( );
+		}),
+
+		GetInputOptions i => Task.Run(async ( ) =>
+		{
+			Console.WriteLine(await GetInputOptions.Run(i));
+			Console.ResetColor( );
+		}),
+	};
+
+	private static Type[ ] LoadVerbs() => Assembly.GetExecutingAssembly( ).GetTypes( )
+		.Where(t => t.GetCustomAttribute<VerbAttribute>( ) != null).ToArray( );
 }
