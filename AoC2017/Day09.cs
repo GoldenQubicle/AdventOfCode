287afc6749842e635bbf526a7fe891fb46cb05e4
vrtxt <goldenqubicle@gmail.com>
2024-01-07 20:33:49 +0100

    2017 day 9

--- AoC2017/Day09.cs
+++ AoC2017/Day09.cs
@@ -2,64 +2,49 @@ namespace AoC2017;
 
 public class Day09 : Solution
 {
-    public Day09(string file) : base(file) { }
-    
-    public Day09(List<string> input) : base(input) { }
+	public Day09(string file) : base(file) { }
 
-    public override async Task<string> SolvePart1( ) => ProcessStream( ).sum;
+	public Day09(List<string> input) : base(input) { }
 
-    public override async Task<string> SolvePart2() => ProcessStream( ).count;
+	public override async Task<string> SolvePart1() => ProcessStream( ).sum;
+
+	public override async Task<string> SolvePart2() => ProcessStream( ).count;
 
 
 	private (string sum, string count) ProcessStream()
-    {
-	    var idx = -1;
-	    var stream = Input.First();
-	    var nesting = 0L;
-	    var sum = 0L;
-	    var inGarbage = false;
-	    var garbageCount = 0;
+	{
+		var idx = -1;
+		var stream = Input.First( );
+		var nesting = 0L;
+		var sum = 0L;
+		var inGarbage = false;
+		var garbageCount = 0;
 
-	    while (idx++ < stream.Length - 1)
-	    {
-		    if (stream[idx] == '!')
-		    {
-			    idx+=1;
-				continue;
-		    }
-
-			if (stream[idx] == '{' && !inGarbage)
-		    {
-			    nesting++;
-			    sum += nesting;
-		    }
-
-		    if (stream[idx] == '}' && !inGarbage)
-		    {
-			    nesting--;
-		    }
-
-		    if (inGarbage)
-		    {
-			    garbageCount++;
-		    }
-
-		    if (stream[idx] == '<')
-		    {
-			    inGarbage = true;
-		    }
-
-		    if (stream[idx] == '>')
-		    {
-			    inGarbage = false;
-		    }
-
-		   
-	    }
-
-	    return (sum.ToString(), garbageCount.ToString());
-    }
-
-	
-
+		while (idx++ < stream.Length - 1)
+		{
+			switch (stream[idx])
+			{
+				case '!':
+					idx++;
+					break;
+				case '>':
+					inGarbage = false;
+					break;
+				case var _ when inGarbage:
+					garbageCount++;
+					break;
+				case '<':
+					inGarbage = true;
+					break;
+				case '{' when !inGarbage:
+					nesting++;
+					sum += nesting;
+					break;
+				case '}' when !inGarbage:
+					nesting--;
+					break;
+			}
+		}
+		return (sum.ToString( ), garbageCount.ToString( ));
+	}
 }
