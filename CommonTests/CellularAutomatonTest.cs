c520df647454be26b8a3ff74c956ec29c5ad6788
vrtxt <goldenqubicle@gmail.com>
2024-04-28 16:50:17 +0200

    refactor common ca2d test

--- CommonTests/CellularAutomatonTest.cs
+++ CommonTests/CellularAutomatonTest.cs
@@ -1,27 +1,26 @@
-﻿using NUnit.Framework;
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace CommonTests
 {
     public class CellularAutomatonTest
     {
         [Test]
-        public void FirstUseCase2015Day18( )
+        public async Task UseCase2015Day18( )
         {
             var day18 = new AoC2015.Day18("day18");
-            var part1 = day18.SolvePart1().Result;
-            var part2 = day18.SolvePart2().Result;
-            Assert.AreEqual("814", part1);
-            Assert.AreEqual("924", part2);
+            
+            Assert.AreEqual("814", await day18.SolvePart1( ));
+            Assert.AreEqual("924", await day18.SolvePart2( ));
         }
 
         [Test]
-        public void UseCase2020Day11()
+        public async Task UseCase2020Day11()
         {
 	        var day18 = new AoC2020.Day11("day11");
-	        //var part1 = day18.SolvePart1( ).Result;
-            var part2 = day18.SolvePart2( ).Result;
-            //Assert.AreEqual("2222", part1);
-            Assert.AreEqual("2032", part2);
+	        
+            Assert.AreEqual("2222", await day18.SolvePart1( ));
+            Assert.AreEqual("2032", await day18.SolvePart2( ));
         }
 	}
 }
