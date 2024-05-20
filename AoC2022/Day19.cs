940183745be3c5d1883eb13240166277e5d1bd33
vrtxt <goldenqubicle@gmail.com>
2024-05-20 09:12:25 +0200

    2022 day 19 stuck

--- AoC2022/Day19.cs
+++ AoC2022/Day19.cs
@@ -1,24 +1,30 @@
+using static AoC2022.Day19;
+
 namespace AoC2022;
+
+using Blueprint = Dictionary<Resource, List<(Resource resource, int amount)>>;
+using Resources = Dictionary<Resource, int>;
 
 public class Day19 : Solution
 {
-	private readonly List<Dictionary<Resource, List<(Resource r, int n)>>> blueprints;
+	public readonly List<Blueprint> Blueprints;
 
 	public Day19(string file) : base(file, split: "\n")
 	{
-		blueprints = Input
-			.Select(l => Regex.Matches(l, @"(\d+)"))
-			.Select(m =>
+		Blueprints = Input.Select(ToBluePrint).ToList( );
+	}
 
-				new Dictionary<Resource, List<(Resource r, int n)>>
-				{
-					{ Resource.Ore , new() { (Resource.Ore, m.AsInt(1)) } },
-					{ Resource.Clay , new() { (Resource.Ore, m.AsInt(2)) } },
-					{ Resource.Obsidian , new () { (Resource.Ore, m.AsInt(3)), (Resource.Clay, m.AsInt(4)) } },
-					{ Resource.Geode , new (){ (Resource.Ore, m.AsInt(5)), (Resource.Obsidian, m.AsInt(6)) } },
-				}
-			).ToList( );
 
+	public Blueprint ToBluePrint(string line)
+	{
+		var matches = Regex.Matches(line, @"(\d+)");
+		return new Blueprint
+		{
+			{ Resource.Ore, new() { (Resource.Ore, matches.AsInt(1)) } },
+			{ Resource.Clay, new() { (Resource.Ore, matches.AsInt(2)) } },
+			{ Resource.Obsidian, new() { (Resource.Ore, matches.AsInt(3)), (Resource.Clay, matches.AsInt(4)) } },
+			{ Resource.Geode, new() { (Resource.Ore, matches.AsInt(5)), (Resource.Obsidian, matches.AsInt(6)) } },
+		};
 	}
 
 
@@ -26,20 +32,59 @@ public class Day19 : Solution
 	{
 		var qualityLevels = new List<int>( );
 
-		foreach (var bluePrint in blueprints.WithIndex( ))
+		foreach (var bluePrint in Blueprints.WithIndex( ))
 		{
+			var qualityLevel = 0;
 			var state = new ProductionState(bluePrint.Value);
-
-			while (state.Minute <= 24)
+			var queue = new PriorityQueue<ProductionState, int>( );
+			queue.Enqueue(state, state.CalculateProductionTime(Resource.Geode));
+			//Console.WriteLine($"Start processing Blueprint {bluePrint.idx + 1}");
+			while (queue.TryDequeue(out var current, out var priority))
 			{
-				state.Tick( );
+				//Console.WriteLine($"queue count: {queue.Count} with current priority {priority}");
+				//Console.WriteLine(current.Print());
+				if (current.Minute == 25)
+				{
+					var level = current.Resources[Resource.Geode] * (bluePrint.idx + 1);
+					qualityLevel = level > qualityLevel ? level : qualityLevel;
+					//Console.WriteLine($"Found level {level}, qualityLevel is now {qualityLevel}");
+					continue;
+				};
+
+				if (current.NoPointContinuing(qualityLevel))
+				{
+					continue;
+				}
+
+				var states = current.GetOptions( ).Select(r =>
+				{
+					var newState = current with
+					{
+						BluePrints = current.BluePrints,
+						Minute = current.Minute,
+						Resources = new(current.Resources),
+						Robots = new(current.Robots),
+						NewRobot = new( )
+					};
+					newState.ProducedRobot(r);
+					return newState;
+				}).ToList( ).Expand(current);
+
+				foreach (var s in states)
+				{
+					s.Tick( );
+
+					queue.Enqueue(s, s.CalculateProductionTime(Resource.Geode));
+				}
+
 			}
 
-			qualityLevels.Add((bluePrint.idx + 1) * state.Resources[Resource.Geode]);
+			qualityLevels.Add(qualityLevel);
 		}
 
 
-
+		//402 too low
+		//617 too low
 		return qualityLevels.Sum( ).ToString( );
 	}
 
@@ -48,68 +93,112 @@ public class Day19 : Solution
 		return string.Empty;
 	}
 
-	internal class ProductionState(Dictionary<Resource, List<(Resource resource, int amount)>> blueprint)
+	public record ProductionState(Blueprint blueprint)
 	{
 		public int Minute { get; set; } = 1;
-		internal Dictionary<Resource, int> Resources { get; init; } = new( )
+		public Resources Resources { get; set; } = new( )
 		{
 			{ Resource.Ore, 0 },{ Resource.Clay, 0 },{ Resource.Obsidian, 0 },{ Resource.Geode, 0 },
 		};
-		internal Dictionary<Resource, int> Robots { get; init; } = new( ) { { Resource.Ore, 1 } };
-		internal Dictionary<Resource, List<(Resource resource, int amount)>> BluePrints { get; init; } = blueprint;
-		internal List<Resource> NewRobots { get; set; } = new();
+		public Resources Robots { get; set; } = new( )
+		{
+			{ Resource.Ore, 1 },{ Resource.Clay, 0 },{ Resource.Obsidian, 0 },{ Resource.Geode, 0 },
+		};
 
+		internal Blueprint BluePrints { get; init; } = blueprint;
+		public List<Resource> NewRobot { get; set; } = new( );
+
+		public string Print()
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine($"Minute {Minute} | Time to Geode {CalculateProductionTime(Resource.Geode)} ");
+			sb.AppendLine($"Robots: Ore {Robots[Resource.Ore]} | Clay {Robots[Resource.Clay]} | Obsidian {Robots[Resource.Obsidian]}  | Geode {Robots[Resource.Geode]} |");
+			sb.AppendLine($"Resources: Ore {Resources[Resource.Ore]} | Clay {Resources[Resource.Clay]} | Obsidian {Resources[Resource.Obsidian]}  | Geode {Resources[Resource.Geode]} |");
+			sb.AppendLine("---------------------------------------------------------");
+			return sb.ToString();
+		}
 
 		public void Tick()
 		{
-			var sb = new StringBuilder( );
-			sb.AppendLine($"==Minute {Minute}==");
-
-			TryProducedRobots(sb);
-
 			foreach (var robot in Robots)
 			{
-				Resources[robot.Key]++;
+				Resources[robot.Key] += robot.Value;
 			}
 
-			if (NewRobots.Count > 0)
-			{
-				NewRobots.ForEach(r => Robots[r] = Robots.ContainsKey(r) ? Robots[r]+1 : 1 );
-				NewRobots.Clear( );
-			}
-
-
-			Robots.ForEach(r => sb.AppendLine($"{r.Value} {r.Key}-collecting robot"));
-			Resources.ForEach(r => sb.Append($" {r.Value} {r.Key} "));
-			sb.AppendLine();
-			Console.WriteLine(sb.ToString());
+			NewRobot.ForEach(r => Robots[r] += 1);
+			NewRobot.Clear( );
 			Minute++;
 		}
 
-		private void TryProducedRobots(StringBuilder sb)
+		public bool NoPointContinuing(int qualityLevel)
 		{
-			foreach (var bluePrint in BluePrints.Reverse())
-			{
-				//this is obviously where the optimization needs to occur
-				if (bluePrint.Value.All(b => Resources[b.resource] >= b.amount))
-				{
-					bluePrint.Value.ForEach(r =>
-					{
-						Resources[r.resource] -= r.amount;
-						sb.Append($"Spend {r.amount} {r.resource}");
-					});
+			// so theory is, the answer to blueprint 2 cannot be found because I was pruning the branch too early
+			// however by increasing the magic 25 to 35 the running time has increase to be unmanageable
+			// in other words, the trick is to come up with a good pruning strategy
+			//return false;
+			var timeTillGeode = CalculateProductionTime(Resource.Geode);
+			var potentialGeodes = 25 - (Minute + timeTillGeode);
+			//return  Minute + timeTillGeode > 25 ;
+			
+			return (Robots[Resource.Ore] > 5 ||   Robots[Resource.Geode] == 0 && 32 - Minute <= qualityLevel);
+		}
 
-					sb.Append($" to build {bluePrint.Key} robot");
-					sb.AppendLine();
-					NewRobots.Add(bluePrint.Key);
-				}
+		public List<Resource> GetOptions()
+		{
+			var possible = Enum.GetValues<Resource>().Where(CanProduceRobot).ToList();
+			if (possible.Contains(Resource.Ore) && Robots[Resource.Ore] == BluePrints[Resource.Ore].First().amount)
+			{
+				possible.Remove(Resource.Ore);
 			}
+
+			return possible;
+		}
+
+
+		public int CalculateProductionTime(Resource robot)
+		{
+			if (CanProduceRobot(robot))
+				return 0 - Robots[robot];
+
+			var isProduced = BluePrints[robot].All(r => Robots[r.resource] > 0);
+
+			if (isProduced)
+			{
+				return BluePrints[robot].Max(r => (r.amount - Resources[r.resource]) / Robots[r.resource]);
+			}
+
+			//so this is N ticks until the missing robot(s) are ready
+			//HOWEVER, production of said robot(s) consumes resources, which in turn has an effect on the production time of geode robot
+			//in other words, to calculate it properly, the state would need to be forwarder, all options considered recursively, etc
+			//at the moment I'll keep it like this as a heuristic to use as priority in the queue, not as actual time.
+			var ticksUntilReady = BluePrints[robot]
+				.Where(r => Robots[r.resource] == 0)
+				.Select(r => CalculateProductionTime(r.resource) + r.amount)
+				.Max( );
+
+			return ticksUntilReady;
+
+			return -1;
+		}
+
+		private bool CanProduceRobot(Resource robot) =>
+			BluePrints[robot].All(r => Resources[r.resource] >= r.amount);
+
+
+		public void ProducedRobot(Resource robot)
+		{
+			foreach (var (resource, amount) in BluePrints[robot])
+			{
+				Resources[resource] -= amount;
+			}
+
+			NewRobot.Add(robot);
 		}
 	}
 
 
 
-	internal enum Resource
+	public enum Resource
 	{
 		Ore,
 		Clay,
--- AoC2022Tests/Day19Test.cs
+++ AoC2022Tests/Day19Test.cs
@@ -1,27 +1,79 @@
+using static AoC2022.Day19;
+
 namespace AoC2022Tests;
-            
+
+using Blueprint = Dictionary<Resource, List<(Resource resource, int amount)>>;
+using Resources = Dictionary<Resource, int>;
+
+
 public class Day19Test
 {
-    Day19 day19;
-            
-    [SetUp]
-    public void Setup( )
-    {
-        day19 = new Day19("day19test1");
-    }
-    
-    [Test]
-    public async Task Part1( )
-    {
-        var actual = await day19.SolvePart1( );
-        Assert.That(actual, Is.EqualTo("33"));
-    }
-            
-    [Test]
-    public async Task Part2( )
-    {
-        var expected = string.Empty;
-        var actual = await day19.SolvePart2( );
-        Assert.That(actual, Is.EqualTo(expected));
-    }
+	Day19 day19;
+
+	[SetUp]
+	public void Setup()
+	{
+		day19 = new Day19("day19test1");
+	}
+
+	[Test]
+	public async Task Part1()
+	{
+		var actual = await day19.SolvePart1( );
+		Assert.That(actual, Is.EqualTo("33"));
+	}
+
+	[Test]
+	public async Task Part2()
+	{
+		var expected = string.Empty;
+		var actual = await day19.SolvePart2( );
+		Assert.That(actual, Is.EqualTo(expected));
+	}
+
+	[TestCaseSource(nameof(ProductionTimeTestCases))]
+	public async Task ProductionStateShouldCalculateTimeUntilGeodeRobot((Resources resources, Resources robots, int expected) test)
+	{
+
+		var state = new ProductionState(ProductionTimeTestBluePrint)
+		{
+			Resources = test.resources,
+			Robots = test.robots
+		};
+
+		var actual = state.CalculateProductionTime(Resource.Geode);
+
+		Assert.That(actual, Is.EqualTo(test.expected));
+	}
+
+	private Blueprint ProductionTimeTestBluePrint =>
+		new( )
+		{
+			{ Resource.Ore,      new() { (Resource.Ore, 2) } },
+			{ Resource.Clay,     new() { (Resource.Ore, 3) } },
+			{ Resource.Obsidian, new() { (Resource.Ore, 4), (Resource.Clay, 5)} },
+			{ Resource.Geode,    new() { (Resource.Ore, 6), (Resource.Obsidian, 7) } },
+		};
+
+	private static IEnumerable<(Resources resources, Resources robots, int expected)> ProductionTimeTestCases()
+	{
+		yield return (new( ) { { Resource.Ore, 6 }, { Resource.Obsidian, 7 } },
+					  new( ) { { Resource.Geode, 1 } }, -1);
+
+		yield return (new( ) { { Resource.Ore, 0 }, { Resource.Obsidian, 0 } },
+					  new( ) { { Resource.Ore, 1 }, { Resource.Obsidian, 1 } }, 7);
+
+		yield return (new( ) { { Resource.Ore, 0 }, { Resource.Obsidian, 0 } },
+					  new( ) { { Resource.Ore, 2 }, { Resource.Obsidian, 3 } }, 3);
+		
+		yield return (new( ) { { Resource.Ore, 4 }, { Resource.Obsidian, 8 } },
+				      new( ) { { Resource.Ore, 2 }, { Resource.Obsidian, 1 } }, 1);
+
+		yield return (new( ) { { Resource.Ore, 0 }, { Resource.Clay, 0 }, { Resource.Obsidian, 0 } },
+					  new( ) { { Resource.Ore, 1 }, { Resource.Clay, 1 }, { Resource.Obsidian, 0 } }, 12);
+
+		yield return (new( ) { { Resource.Ore, 0 }, { Resource.Clay, 0 }, { Resource.Obsidian, 0 } },
+					  new( ) { { Resource.Ore, 1 }, { Resource.Clay, 0 }, { Resource.Obsidian, 0 } }, 15);
+	}
+
 }
