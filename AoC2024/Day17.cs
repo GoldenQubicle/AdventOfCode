f8a00fec2aa930056fb25d5f05c56b0be9574cc3
vrtxt <goldenqubicle@gmail.com>
2024-12-17 22:34:27 +0100

    2024 day 17

--- AoC2024/Day17.cs
+++ AoC2024/Day17.cs
@@ -2,36 +2,133 @@ namespace AoC2024;
 
 public class Day17 : Solution
 {
-	private readonly Dictionary<char, int> registers = new( );
-	private readonly List<int> program;
+	public readonly Dictionary<char, long> registers = new( );
+	private List<long> program;
 
 	private const char A = 'A';
 	private const char B = 'B';
 	private const char C = 'C';
 
-	public Day17(string file) : base(file)
+	public Day17(string file) : base(file) => SetRegisters( );
+
+	public Day17(List<string> input) : base(input) => SetRegisters( );
+
+
+	private void SetRegisters()
 	{
+		registers.Clear( );
 		registers.Add(A, Input[0].ToInt( ));
 		registers.Add(B, Input[1].ToInt( ));
 		registers.Add(C, Input[2].ToInt( ));
 
-		program = Input[3].Split(':')[1].Split(',', StringSplitOptions.TrimEntries).Select(int.Parse).ToList( );
-
+		program = Input[3].Split(':')[1].Split(',', StringSplitOptions.TrimEntries).Select(long.Parse).ToList( );
 	}
 
 
-	public override async Task<string> SolvePart1()
+	public override async Task<string> SolvePart1() => string.Join(",", RunProgram( ));
+
+	public override async Task<string> SolvePart2()
 	{
-		var pointer = 0;
-		var output = new List<int>();
-		while (pointer < program.Count)
+		//basically started all variables from zero to find output up to idx 13
+		//however from there the increment overshot consistently so...
+		//hardcoded the starting values from that point onwards, and proceeded with a smaller increment
+		//the test will fail miserably but that is fine
+		var idx = 13;
+		var aValue = 60655122332687L;
+		var inc = 1073741824L;
+		
+		while (true) // no exit condition. answer found manually stepping through
+		{
+			SetRegisters( );
+			registers[A] = aValue;
+			var output = RunProgram( );
+
+			if (output.Take(idx + 1).Zip(program.Take(idx + 1), (l, l1) => l == l1).All(b => b))
+			{
+				idx++;
+
+				if(output.Count < program.Count-3)
+					inc = getIncrement(idx);
+				
+			}
+
+			aValue += inc;
+
+		}
+
+		return aValue.ToString( );
+
+		long getIncrement(int idx) => idx switch
+		{
+			0 => 1,
+			1 => 8,
+			2 => 64,
+			3 => 512,
+			4 => 4096,
+			5 => 32768,
+			6 => 262144,
+			7 => 2097152,
+			8 => 16777216,
+			9 => 134217728,
+			10 => 1073741824,
+			11 => 8589934592,
+			12 => 68719476736,
+			13 => 549755813888,
+			14 => 4398046511104,
+			15 => 35184372088832,
+			16 => 281474976710656,
+		};
+
+		long Get8BaseSum(string output)
+		{
+			var sum = 0;
+			for (var idx = 0 ;idx < output.Length ;idx += 2)
+			{
+				sum += output[idx].ToInt( ) * idx switch
+				{
+					0 => 8,
+					2 => 64,
+					4 => 512,
+					6 => 4096,
+					8 => 32768,
+					10 => 262144
+				};
+			}
+
+			return sum;
+		}
+
+		/*
+		so we're clearly counting in some binary base 8 system.
+		that is, the 1st number in the output is
+			0 at count 0-7
+			1 at count 8-15
+			2 at count 16-31
+
+		the 2nd number in the output is
+			0 at count 8-63
+			1 at count 64-127
+			2 at count 128-191
+
+		however this pattern does not repeat nicely in the input...
+		 */
+	}
+
+
+
+	private List<long> RunProgram()
+	{
+		var pointer = 0L;
+		var output = new List<long>( );
+		var isDone = false;
+		while (pointer < program.Count && !isDone)
 		{
 			var (opcode, operand) = Read(pointer);
 
 			switch (opcode)
 			{
 				case OpCode.Adv:
-					registers[A] = (int)Math.Truncate(registers[A] / Math.Pow(2, operand));
+					registers[A] = (long)Math.Truncate(registers[A] / Math.Pow(2, operand));
 					pointer += 2;
 					break;
 				case OpCode.Bxl:
@@ -44,7 +141,9 @@ public class Day17 : Solution
 					break;
 				case OpCode.Jnz:
 					if (registers[A] > 0)
-						pointer += operand;
+						pointer = operand;
+					else
+						isDone = true;
 					break;
 				case OpCode.Bcx:
 					registers[B] ^= registers[C];
@@ -55,44 +154,45 @@ public class Day17 : Solution
 					pointer += 2;
 					break;
 				case OpCode.Bdv:
-					registers[B] = (int)Math.Truncate(registers[A] / Math.Pow(2, operand));
+					registers[B] = (long)Math.Truncate(registers[A] / Math.Pow(2, operand));
 					pointer += 2;
 					break;
 				case OpCode.Cdv:
-					registers[C] = (int)Math.Truncate(registers[A] / Math.Pow(2, operand));
+					registers[C] = (long)Math.Truncate(registers[A] / Math.Pow(2, operand));
+					pointer += 2;
 					break;
 				default:
-					throw new ArgumentOutOfRangeException();
+					throw new ArgumentOutOfRangeException( );
 			}
 		}
 
-		
-
-		return string.Join(",", output);
+		//return empty string to support a few unit test cases.
+		return output;
 	}
 
-	private (OpCode opcode, int operand) Read(int pointer)
+
+
+	private (OpCode opcode, long operand) Read(long pointer)
 	{
-		var opcode = (OpCode)program[pointer];
+		var opcode = (OpCode)program[(int)pointer];
 		var operand = GetOperand(opcode, pointer);
 		return (opcode, operand);
 	}
 
-	private int GetOperand(OpCode opcode, int pointer)
+	private long GetOperand(OpCode opcode, long pointer)
 	{
 		var isLiteral = opcode is OpCode.Bxl or OpCode.Jnz;
-		var operand = program[pointer + 1];
-
+		var operand = program[(int)pointer + 1];
 		return isLiteral ? operand : GetCombo(operand);
 	}
 
-	private int GetCombo(int operand) => operand switch
+	private long GetCombo(long operand) => operand switch
 	{
-		>= 0 and <= 3 => operand,
+		<= 3 => operand,
 		4 => registers[A],
 		5 => registers[B],
 		6 => registers[C],
-		_ => throw new ArgumentOutOfRangeException()
+		_ => throw new ArgumentOutOfRangeException( )
 	};
 
 	private enum OpCode
@@ -100,8 +200,5 @@ public class Day17 : Solution
 		Adv, Bxl, Bst, Jnz, Bcx, Out, Bdv, Cdv
 	}
 
-	public override async Task<string> SolvePart2()
-	{
-		return string.Empty;
-	}
+
 }
--- AoC2024Tests/Day17Test.cs
+++ AoC2024Tests/Day17Test.cs
@@ -2,13 +2,13 @@ namespace AoC2024Tests;
             
 public class Day17Test
 {
-    Day17 day17;
+    //Day17 day17;
             
-    [SetUp]
-    public void Setup( )
-    {
-        //day17 = new Day17("day17test1");
-    }
+    //[SetUp]
+    //public void Setup( )
+    //{
+    //    //day17 = new Day17("day17test1");
+    //}
 
 	[TestCaseSource(nameof(GetCasesPart1))]
 	public async Task Part1((string file, string expected) testCase)
@@ -17,13 +17,33 @@ public class Day17Test
 		var actual = await day17.SolvePart1( );
         Assert.That(actual, Is.EqualTo(testCase.expected));
     }
+
+    [TestCaseSource(nameof(GetRegisterTestCases))]
+	public void TestRegisters((List<string> input, char register, int expected) test)
+	{
+        var day17 = new Day17(test.input);
+
+        day17.SolvePart1();
+
+        Assert.That(day17.registers[test.register], Is.EqualTo(test.expected));
+	}
             
     [Test]
     public async Task Part2( )
     {
-        var actual = await day17.SolvePart2( );
-        Assert.That(actual, Is.EqualTo(""));
+	    var day17 = new Day17(["2024", "0", "0", " :0,3,5,4,3,0"]);
+	    var actual = await day17.SolvePart2();
+	    Assert.That(actual, Is.EqualTo("117440"));
     }
+
+    private static IEnumerable<(List<string> input, char register, int expected)> GetRegisterTestCases()
+    {
+	    yield return (["0", "0", "9", " : 2,6"], 'B', 1);
+	    yield return (["0", "29", "0", " : 1,7"], 'B', 26);
+	    yield return (["0", "2024", "43690", " : 4,0"], 'B', 44354);
+	    yield return (["2024", "0", "43690", " : 0,1,5,4,3,0"], 'A', 0);
+    }
+
 
     private static IEnumerable<(string file, string expected)> GetCasesPart1()
     {
