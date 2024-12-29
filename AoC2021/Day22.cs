f9f2e59f1b6ac84c84848ff683bf6c4d9ced6f6d
vrtxt <goldenqubicle@gmail.com>
2024-12-29 18:50:06 +0100

    2021 day 22

--- AoC2021/Day22.cs
+++ AoC2021/Day22.cs
@@ -2,101 +2,78 @@ namespace AoC2021;
 
 public class Day22 : Solution
 {
-	private readonly List<Cuboid> cuboids;
-	private Dictionary<Vector3, bool> reactor = new( );
+	private List<Cuboid> cuboids;
 
-	public Day22(string file) : base(file, split: "\n")
+	public Day22(string file) : base(file, split: "\n") => cuboids = Input
+		.Select(l => Regex.Match(l, @"(?<state>on|off) x=(?<x>-?\d+..-?\d+),y=(?<y>-?\d+..-?\d+),z=(?<z>-?\d+..-?\d+)"))
+		.Select(m => new Cuboid(m.AsString("state"), ToRange(m.AsString("x")), ToRange(m.AsString("y")), ToRange(m.AsString("z"))))
+		.ToList( );
+
+	public override async Task<string> SolvePart1() => DoReactorReboot(isPart1: true);
+
+	public override async Task<string> SolvePart2() => DoReactorReboot(isPart1: false);
+
+	private string DoReactorReboot(bool isPart1)
 	{
-		cuboids = Input
-			.Select(l => Regex.Match(l, @"(?<state>on|off) x=(?<x>-?\d+..-?\d+),y=(?<y>-?\d+..-?\d+),z=(?<z>-?\d+..-?\d+)"))
-			.Select(m => new Cuboid(m.AsString("state"), ToRange(m.AsString("x")), ToRange(m.AsString("y")), ToRange(m.AsString("z"))))
-			.ToList( );
-	}
+		var volumes = new List<Cuboid>( );
+		var region = new Cuboid("", (-50, 50), (-50, 50), (-50, 50));
+		cuboids = isPart1 ? cuboids.Where(c => c.HasOverlap(region)).ToList( ) : cuboids;
 
-
-	public override async Task<string> SolvePart1()
-	{
-		cuboids.ForEach(s =>
+		foreach (var cuboid in cuboids)
 		{
-			foreach (var point in s.GetPoints( ))
-			{
-				if (reactor.ContainsKey(point))
-					reactor[point] = s.GetState( );
-				else
-					reactor.Add(point, s.GetState( ));
-			}
-		});
+			if (cuboid.State == "on")
+				volumes.Add(cuboid);
 
-		return reactor.Count(kvp => kvp.Value).ToString( );
+			var overlaps = volumes.Where(c => c.HasOverlap(cuboid)).ToList( );
+			if (overlaps.Count == 0)
+				continue;
+
+			volumes.AddRange(overlaps.Select(c => c.GetOverlap(cuboid)));
+		}
+
+		return volumes.Aggregate(0L, (v, c) => c.State == "on" ? v + c.Volume : v - c.Volume).ToString( );
 	}
 
-
-	public override async Task<string> SolvePart2()
+	private static (long min, long max) ToRange(string match)
 	{
-		return string.Empty;
-	}
-
-	private (int min, int max) ToRange(string match)
-	{
-		var parts = match.Split("..", StringSplitOptions.TrimEntries).Select(int.Parse).ToList( );
+		var parts = match.Split("..", StringSplitOptions.TrimEntries).Select(long.Parse).ToList( );
 		var min = parts[0] < parts[1] ? parts[0] : parts[1];
 		var max = min == parts[0] ? parts[1] : parts[0];
 		return (min, max);
 	}
 
-	public static bool Intersects(Cuboid c1, Cuboid c2)
+	public record Cuboid(string State, (long min, long max) X, (long min, long max) Y, (long min, long max) Z)
 	{
-		if (!(c1.X.min <= c2.X.max && c1.X.max >= c2.X.min))
-			return false;
+		public long Volume = (Math.Abs(X.max - X.min) + 1) * (Math.Abs(Y.max - Y.min) + 1) * (Math.Abs(Z.max - Z.min) + 1);
 
-		if (!(c1.Y.min <= c2.Y.max && c1.Y.max >= c2.Y.min))
-			return false;
-
-		if (!(c1.Z.min <= c2.Z.max && c1.Z.max >= c2.Z.min))
-			return false;
-
-		return true;
-	}
-
-	public static Cuboid GetIntersectVolume(Cuboid c1, Cuboid c2)
-	{
-		var min_x = Math.Max(c1.X.min, c2.X.min);
-		var max_x = Math.Min(c1.X.max, c2.X.max);
-		var min_y = Math.Max(c1.Y.min, c2.Y.min);
-		var max_y = Math.Min(c1.Y.max, c2.Y.max);
-		var min_z = Math.Max(c1.Z.min, c2.Z.min);
-		var max_z = Math.Min(c1.Z.max, c2.Z.max);
-
-		var state = c2.GetState( ) ? "off" : "on";
-
-		return new Cuboid(state, (min_x, max_x), (min_y, max_y), (min_z, max_z));
-	}
-
-	public record Cuboid(string State, (int min, int max) X, (int min, int max) Y, (int min, int max) Z)
-	{
-		public List<Vector3> GetPoints()
+		public bool HasOverlap(Cuboid other)
 		{
-			if (!(OverlapsRegion(X) && OverlapsRegion(Y) && OverlapsRegion(Z)))
-				return new List<Vector3>( );
+			if (other == this)
+				return false;
 
-			return Enumerable.Range(X.min, X.max - X.min + 1)
-				.SelectMany(x => Enumerable.Range(Y.min, Y.max - Y.min + 1)
-					.SelectMany(y => Enumerable.Range(Z.min, Z.max - Z.min + 1)
-							.Select(z => new Vector3(x, y, z)))).ToList( );
+			if (!(X.min <= other.X.max && X.max >= other.X.min))
+				return false;
+
+			if (!(Y.min <= other.Y.max && Y.max >= other.Y.min))
+				return false;
+
+			if (!(Z.min <= other.Z.max && Z.max >= other.Z.min))
+				return false;
+
+			return true;
 		}
 
-		public bool GetState() => State switch
+		public Cuboid GetOverlap(Cuboid other)
 		{
-			"on" => true,
-			"off" => false,
-			_ => throw new ArgumentException( )
-		};
-
-		public int GetVolume() => Math.Abs(X.max - X.min) * Math.Abs(Y.max - Y.min) * Math.Abs(Z.max - Z.min);
-
-		private bool OverlapsRegion((int min, int max) range) =>
-			range.min is >= -50 and <= 50 ||
-			range.max is >= -50 and <= 50 ||
-			range is { min: < -50, max: > 50 };
+			var min_x = Math.Max(X.min, other.X.min);
+			var max_x = Math.Min(X.max, other.X.max);
+			var min_y = Math.Max(Y.min, other.Y.min);
+			var max_y = Math.Min(Y.max, other.Y.max);
+			var min_z = Math.Max(Z.min, other.Z.min);
+			var max_z = Math.Min(Z.max, other.Z.max);
+			//had a nested ternary to determine state however it collapsed down into this
+			var state = State == "on" ? "off" : "on";
+			return new Cuboid(state, (min_x, max_x), (min_y, max_y), (min_z, max_z));
+		}
 	}
 }
--- AoC2021Tests/Day22Test.cs
+++ AoC2021Tests/Day22Test.cs
@@ -1,56 +1,63 @@
 namespace AoC2021Tests;
-            
+
 public class Day22Test
 {
-    Day22 day22;
-            
-    [SetUp]
-    public void Setup( )
-    {
-        day22 = new Day22("day22test1");
-    }
-    
-    [Test]
-    public async Task Part1( )
-    {
-        var actual = await day22.SolvePart1( );
-        Assert.That(actual, Is.EqualTo("590784"));
-    }
-            
-    [Test]
-    public async Task Part2( )
-    {
-        var expected = string.Empty;
-        var actual = await day22.SolvePart2( );
-        Assert.That(actual, Is.EqualTo(expected));
-    }
-
-    [TestCaseSource(nameof(GetIntersectionTestCases))]
-    public void CuboidIntersectTest((Day22.Cuboid step, bool expected) test)
-    {
-	    var c1 = new Day22.Cuboid("on", (-1, 1), (-1, 1), (-1, 1));
-	    var result = Day22.Intersects(c1, test.step);
-        Assert.That(result, Is.EqualTo(test.expected));
+	[TestCaseSource(nameof(GetTestCasesPart1))]
+	public async Task Part1((string file, string expected) test)
+	{
+		var day22 = new Day22(test.file);
+		var actual = await day22.SolvePart1( );
+		Assert.That(actual, Is.EqualTo(test.expected));
 	}
 
-    [TestCaseSource(nameof(GetIntersectionTestCases))]
+	[Test]
+	public async Task Part2()
+	{
+		var day22 = new Day22("day22test3");
+		var actual = await day22.SolvePart2( );
+		Assert.That(actual, Is.EqualTo("2758514936282235"));
+	}
+
+	[TestCaseSource(nameof(GetIntersectionTestCases))]
+	public void CuboidIntersectTest((Day22.Cuboid step, bool expected) test)
+	{
+		var c1 = new Day22.Cuboid("on", (-1, 1), (-1, 1), (-1, 1));
+		var result = c1.HasOverlap(test.step);
+		Assert.That(result, Is.EqualTo(test.expected));
+	}
+
+	[TestCaseSource(nameof(GetIntersectionTestCases))]
 	public void CuboidIntersectVolumeTest((Day22.Cuboid step, bool expected) test)
 	{
-		if (test.expected == false) return;
+		if (test.expected == false)
+			return;
 
 		var c1 = new Day22.Cuboid("on", (-1, 1), (-1, 1), (-1, 1));
-		var result = Day22.GetIntersectVolume(c1, test.step);
-        Assert.That(result.GetVolume(), Is.EqualTo(4));
+		var result = c1.GetOverlap(test.step);
+		Assert.That(result.Volume, Is.EqualTo(18));
 	}
 
-    public static IEnumerable <(Day22.Cuboid, bool)> GetIntersectionTestCases()
-    {
-	    yield return (new Day22.Cuboid("on", (0, 2), (-2, 2), (-2, 2)), true);
-	    yield return (new Day22.Cuboid("on", (10, 12), (-2, 2), (-2, 2)), false);
+	public static IEnumerable<(string file, string expected)> GetTestCasesPart1()
+	{
+		yield return ("day22test1", "590784");
+		yield return ("day22test2", "39");
+	}
+
+	public static IEnumerable<(Day22.Cuboid, bool)> GetIntersectionTestCases()
+	{
+		yield return (new Day22.Cuboid("on", (0, 2), (-2, 2), (-2, 2)), true);
+		yield return (new Day22.Cuboid("on", (10, 12), (-2, 2), (-2, 2)), false);
 		yield return (new Day22.Cuboid("on", (-2, 2), (0, 2), (-2, 2)), true);
 		yield return (new Day22.Cuboid("on", (-2, 2), (10, 12), (-2, 2)), false);
-	    yield return (new Day22.Cuboid("on", (-2, 2), (-2, 2), (0, 2)), true);
-	    yield return (new Day22.Cuboid("on", (-2, 2), (-2, 2), (10, 12)), false);
-	    
+		yield return (new Day22.Cuboid("on", (-2, 2), (-2, 2), (0, 2)), true);
+		yield return (new Day22.Cuboid("on", (-2, 2), (-2, 2), (10, 12)), false);
+
+	}
+	[Test]
+	public void VolumeTest()
+	{
+		var c = new Day22.Cuboid("on", (0, 1), (0, 1), (0, 1));
+		var v = c.Volume;
+		Assert.That(v, Is.EqualTo(8));
 	}
 }
